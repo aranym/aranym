da6cd39152d034e10e03194b6926d55ea3348dc9
joy <>
2001-04-03 06:43:05 +0000

    MFPintlev() should return the level of interrupt <0-15>. This should be fixed to allow several simultaneous interrupts pending - currently TimerC resets keyboard events or vice versa!

--- src/uae_cpu/basilisk_glue.cpp
+++ src/uae_cpu/basilisk_glue.cpp
@@ -42,9 +42,10 @@ uint32 ROMSize;		// Size of ROM
 uint32 TTRAMBase = 0x01000000;		// TT-RAM base (Atari address space)
 uint8 *TTRAMBaseHost;	// TT-RAM base (host address space)
 uint32 TTRAMSize = 0;	// Size of TT-RAM
-uint32 VideoRAMBase = 0xf0000000;	// VideoRAM base (Atari address space)
+uint32 VideoRAMBase = ARANYMVRAMSTART;	// VideoRAM base (Atari address space)
 uint8 *VideoRAMBaseHost;// VideoRAM base (host address space)
 uint32 VideoRAMSize;	// Size of VideoRAM
+uint32 MFPInterruptFlags;
 
 uintptr MEMBaseDiff;	// Global offset between a Atari address and its Host equivalent
 uintptr VMEMBaseDiff;	// Global offset between a Atari VideoRAM address and /dev/fb0 mmap
@@ -106,8 +107,9 @@ void TriggerVBL(void)
 	regs.spcflags |= SPCFLAG_DOINT;
 }
 
-void TriggerMFP(void)
+void TriggerMFP(int no)
 {
+	MFPInterruptFlags = no;
 	regs.spcflags |= SPCFLAG_MFPINT;
 }
 
@@ -124,6 +126,15 @@ void TriggerNMI(void)
 int intlev(void)
 {
 	return (InterruptFlags == VBL) ? 4 : 0;
+}
+
+/*
+ *  Get 68k interrupt level
+ */
+
+int MFPintlev(void)
+{
+	return MFPInterruptFlags;
 }
 
 
