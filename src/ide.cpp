5370e149b498d87826e4200353d64e227e7ae6d6
joy <>
2001-04-09 10:06:22 +0000

    more trials on IDE front

--- src/ide.cpp
+++ src/ide.cpp
@@ -6,11 +6,16 @@
 #include "memory.h"
 #include "ide.h"
 
+static bool dP = true;
+
 IDE::IDE() {
-	data = error = status = altstatus = 0;
+	data = error = 0;
+	status = altstatus = 0x50;
 }
 
 static const int HW = 0xf00000;
+
+static uae_u16 DriveInformation[256];
 
 uae_u8 IDE::handleRead(uaecptr addr) {
 	addr -= HW;
@@ -22,9 +27,15 @@ uae_u8 IDE::handleRead(uaecptr addr) {
 		case 1:
 		case 2:
 		case 3: return (data >> (8*(3-addr)));
-		case 5: return error;
-		case 0x1d: return status;
-		case 0x39:	return altstatus;
+		case 5: if (dP)
+					fprintf(stderr, "read error register at %x\n", showPC());
+				return error;
+		case 0x1d: if (dP)
+					fprintf(stderr, "read status register at %x\n", showPC());
+				return status;
+		case 0x39: if (dP)
+					fprintf(stderr, "read ALTstatus register at %x\n", showPC());
+				return altstatus;
 		default: fprintf(stderr, "Unknown IDE register read: %06x\n", HW+addr);
 	}
 
@@ -40,14 +51,33 @@ void IDE::handleWrite(uaecptr addr, uae_u8 value) {
 		case 0:
 		case 1:
 		case 2:
-		case 3: data = (data & (~(0xff000000 >> (8*addr)))) | (value << (8*(3-addr))); break;
+		case 3: data = (data & (~(0xff000000 >> (8*addr)))) | (value << (8*(3-addr)));
+				if (dP)
+					fprintf(stderr, "Written data %08x at %x\n", data, showPC());
+				break;
 		case 9: scount = value; break;
 		case 0xd: snumber = value; break;
 		case 0x11: clow = value; break;
 		case 0x15: chigh = value; break;
-		case 0x19: dhead = value; break;
-		case 0x1d: cmd = value; break;
-		case 0x39: dataout = value; break;
+		case 0x19: dhead = value;
+					if (dP)
+						fprintf(stderr, "Written drive head %d ($%x) at %x\n", value, value, showPC());
+					break;
+		case 0x1d: cmd = value;
+					switch(cmd) {
+						case 0xec:	/* Identify Drive */
+							/* set BSY, store info in sector buffer, set DRQ and generate interrupt */
+							status = 0x08;	// DRQ
+							break;
+
+					}
+					if (dP)
+						fprintf(stderr, "Written command %d ($%x) at %x\n", value, value, showPC());
+					break;
+		case 0x39: dataout = value;
+					if (dP)
+						fprintf(stderr, "Written data out %d ($%x) at %x\n", value, value, showPC());
+					break;
 		default: fprintf(stderr, "Unknown IDE register write: %06x = %d ($%02x)\n", HW+addr, value, value);
 	}
 }
