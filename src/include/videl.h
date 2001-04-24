5d4fa9104fc6ef1f6d2f97bf7c61da04db1c18a5
joy <>
2001-04-24 06:49:48 +0000

    getVideoMode() added - it should be static, but then half of the variables would have to be static and so on. Confusing stuff.

--- src/include/videl.h
+++ src/include/videl.h
@@ -1,17 +1,20 @@
 /* Joy 2001 */
+#ifndef _VIDEL_H
+#define _VIDEL_H
 
 class VIDEL {
 private:
 	uae_u8 shifter;
 	uae_u16 videl;
-	uae_u8 videoctrl, videoctrl2;
+	uae_u16 videoctrl;
 	uae_u8 syncmode;
 	uae_u8 videomode;
 
 public:
-	VIDEL(void);
+	VIDEL();
 	uae_u8 handleRead(uaecptr);
 	void handleWrite(uaecptr, uae_u8);
-
-private:
+	int getVideoMode();
 };
+
+#endif
