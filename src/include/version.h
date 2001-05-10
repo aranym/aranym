3883e506c93b05d9aa4d31157ee2a98ea44ca13f
joy <>
2001-05-10 12:23:55 +0000

    update version information

--- src/include/version.h
+++ src/include/version.h
@@ -22,8 +22,9 @@
 #define VERSION_H
 
 const int VERSION_MAJOR = 0;
-const int VERSION_MINOR = 8;
+const int VERSION_MINOR = 0;
+const int VERSION_SUBMINOR = 1;
 
-#define VERSION_STRING "Basilisk II V0.8"
+#define VERSION_STRING "ARAnyM pre-alpha"
 
 #endif
