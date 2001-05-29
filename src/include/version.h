35005aa99e7190083f559babb9538cf8e7a2b0c0
joy <>
2001-05-29 06:57:46 +0000

    version++

--- src/include/version.h
+++ src/include/version.h
@@ -23,8 +23,8 @@
 
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 0;
-const int VERSION_SUBMINOR = 1;
+const int VERSION_SUBMINOR = 2;
 
-#define VERSION_STRING "ARAnyM pre-alpha"
+#define VERSION_STRING "ARAnyM 0.0.2"
 
 #endif
