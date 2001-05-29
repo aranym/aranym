b96f988fd5391cccc722c5db11794325118ae070
joy <>
2001-05-29 06:55:08 +0000

    new parameters added

--- src/include/parameters.h
+++ src/include/parameters.h
@@ -9,6 +9,8 @@
 extern uint8 start_debug;		// starts debugger
 extern uint8 fullscreen;		// boot in fullscreen
 extern uint16 boot_color_depth;		// boot color depth
+extern bool direct_truecolor;	// patch TOS to enable direct true color
+extern uint8 monitor;				// VGA or TV
 
 extern char *program_name;
 extern char *rom_path;
