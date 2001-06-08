bebcd37bf069edbf365ef64bbd31a808fd5eb313
joy <>
2001-06-08 12:18:57 +0000

    elementar support for inquiring videl screen width and height - unfinished, not fully working. Beware!

--- src/videl.cpp
+++ src/videl.cpp
@@ -8,14 +8,21 @@
 
 static const int HW = 0xff8200;
 
-static bool dP = false;
+static bool dP = true;
 
 extern uae_u32 vram_addr;
 
 VIDEL::VIDEL() {
-	syncmode = videoctrl = videomode = 0xff;
+	videoctrl = videomode = 0xff;
+	syncmode = 2;	// PAL mode, internal sync
 	videl = 0xfffe;
 }
+
+#define LO(a) ((a) & 0x00ff)
+#define HI(a) ((a) >> 8)
+
+#define STORELO(a, b) (((a) & 0xff00) | (b))
+#define STOREHI(a, b) (((a) & 0x00ff) | ((b) << 8))
 
 uae_u8 VIDEL::handleRead(uaecptr addr) {
 	addr -= HW;
@@ -30,11 +37,21 @@ uae_u8 VIDEL::handleRead(uaecptr addr) {
 		case 0x03: return vram_addr >> 8;
 		case 0x0d: return vram_addr;
 		case 0x0a: return syncmode;
+		case 0x10: return HI(linewide);
+		case 0x11: return LO(linewide);
 		case 0x60: return shifter;
-		case 0x66: return videl >> 8;
-		case 0x67: return videl & 0x00ff;
-		case 0xc0: return videoctrl >> 8;
-		case 0xc1: return (videoctrl | 0x03);	// enforce TV
+		case 0x66: return HI(videl);
+		case 0x67: return LO(videl);
+		case 0x88: return HI(hdb);
+		case 0x89: return LO(hdb);
+		case 0x8a: return HI(hde);
+		case 0x8b: return LO(hde);
+		case 0xa8: return HI(vdb);
+		case 0xa9: return LO(vdb);
+		case 0xaa: return HI(vde);
+		case 0xab: return LO(vde);
+		case 0xc0: return HI(videoctrl);
+		case 0xc1: return LO(videoctrl);	// monitor == TV ? videoctrl | 0x03
 		case 0xc3: return videomode;
 		default: return 0;
 	}
@@ -53,6 +70,16 @@ void VIDEL::handleWrite(uaecptr addr, uae_u8 value) {
 		case 0x03: vram_addr = (vram_addr & 0x00ff00ff) | (value << 8); break;
 		case 0x0d: vram_addr = (vram_addr & 0x00ffff00) | value; break;
 		case 0x0a: syncmode = value; break;
+		case 0x10: linewide = (value << 8) | (linewide & 0x00ff); break;
+		case 0x11: linewide = (linewide & 0xff00) | value; break;
+		case 0x88: hdb = STOREHI(hdb, value); break;
+		case 0x89: hdb = STORELO(hdb, value); fprintf(stderr, "hdb = %d\n", hdb); break;
+		case 0x8a: hde = STOREHI(hde, value); break;
+		case 0x8b: hde = STORELO(hde, value); fprintf(stderr, "hde = %d\n", hde); break;
+		case 0xa8: vdb = STOREHI(vdb, value); break;
+		case 0xa9: vdb = STORELO(vdb, value); fprintf(stderr, "vdb = %d\n", vdb); break;
+		case 0xaa: vde = STOREHI(vde, value); break;
+		case 0xab: vde = STORELO(vde, value); fprintf(stderr, "vde = %d\n", vde); break;
 		case 0x60: shifter = value; break;
 		case 0x66: videl = (videl & 0x00ff) | (value << 8); break;
 		case 0x67: videl = (videl & 0xff00) | value; break;
@@ -96,4 +123,18 @@ int VIDEL::getVideoMode() {
 		return 2;
 	else
 		return 4;
+}
+
+int VIDEL::getScreenWidth() {
+	if (linewide != 0)
+		return linewide * 16 / getVideoMode();
+	else
+		return 640;
+}
+
+int VIDEL::getScreenHeight() {
+	if (vdb != 0 && vde - vdb > 0)
+		return (vde - vdb) >> 1;
+	else
+		return 480;
 }
