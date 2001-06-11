cba1c27cc6375e5ddb3f2a9186a9033cc042f5f6
joy <>
2001-06-11 12:22:25 +0000

    videl does not need to modify read of write methods

--- src/include/videl.h
+++ src/include/videl.h
@@ -4,7 +4,8 @@
 
 #include "icio.h"
 
-class VIDEL : public ICio {
+class VIDEL : public BASE_IO {
+/*
 private:
 	uae_u8 shifter;
 	uae_u16 videl;
@@ -14,12 +15,13 @@ private:
 	uae_u16 linewide;
 	uae_u16 hdb, hde;
 	uae_u16 vdb, vde;
-
+*/
 
 public:
-	VIDEL();
-	virtual uae_u8 handleRead(uaecptr);
-	virtual void handleWrite(uaecptr, uae_u8);
+	// VIDEL();
+	// virtual uae_u8 handleRead(uaecptr);
+	// virtual void handleWrite(uaecptr, uae_u8);
+	uaecptr getVideoramAddress();
 	int getVideoMode();
 	int getScreenWidth();
 	int getScreenHeight();
