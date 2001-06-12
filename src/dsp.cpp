a2f7630f1f60d383a9e8066f5914fcc7a79dd7ae
joy <>
2001-06-12 17:14:10 +0000

    simpler code

--- src/dsp.cpp
+++ src/dsp.cpp
@@ -5,20 +5,9 @@
 #include "cpu_emulation.h"
 #include "memory.h"
 #include "dsp.h"
-#include "parameters.h"
-
-DSP::DSP() {
-}
 
 static const int HW = 0xffa200;
 
 uae_u8 DSP::handleRead(uaecptr addr) {
-	addr -= HW;
-	switch(addr) {
-		case 2:	// interrupt status register
-			return 0xff;
-		default:
-			return 0xff;
-			// return BASE_IO::handleRead(addr);
-	}
+	return 0xff;	// this is to prevent the TOS to hang in DSP initialization routine
 }
