6e0fa3a62bd86eff891922685c1844946fd2a268
joy <>
2001-09-04 13:57:51 +0000

    is this used at all yet?

--- src/include/emu_bochs.h
+++ src/include/emu_bochs.h
@@ -2,6 +2,53 @@
 #ifndef _emu_bochs_h
 #define _emu_bochs_h
 
+extern "C" {
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef WIN32
+#  include <unistd.h>
+#else
+#  include <io.h>
+#endif
+#include <time.h>
+#ifdef macintosh
+#  include <types.h>
+#  include <stat.h>
+#  include <utime.h>
+#else
+#  ifndef WIN32
+#    include <sys/time.h>
+#  endif
+#  include <sys/types.h>
+#  include <sys/stat.h>
+#endif
+#include <ctype.h>
+#include <string.h>
+#include <fcntl.h>
+#ifdef macintosh
+#  include "macutils.h"
+#  define SuperDrive "[fd:]"
+#endif
+}
+
+// Hacks for win32: always return regular file.
+#ifdef WIN32
+#ifndef __MINGW32__
+#  define S_ISREG(st_mode) 1
+#  define S_ISCHR(st_mode) 0
+
+// VCPP includes also are missing these
+#  define off_t long
+#  define ssize_t int
+#endif
+
+// win32 has snprintf though with different name.
+#define snprintf _snprintf
+
+#endif
+
+
 // moje triky
 #define put(a)
 #define settype(a)
