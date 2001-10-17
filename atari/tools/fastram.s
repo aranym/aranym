f89fe233f2d1405c2d0424154cdb6ff3afee7838
standa <>
2001-10-17 17:13:40 +0000

    The WWW aratools.tgz archive changes were applied. The README was added to CVS.

--- atari/tools/fastram.s
+++ atari/tools/fastram.s
@@ -6,13 +6,19 @@
 	addq	#6,sp
 	move.l	d0,-(sp)		uschovat USERSTACK pointer na z sobn¡k
 
-	pea	$2000000		32 MB RAM
+	lea	$f90000,a0
+	cmp.l	#$5F415241,(a0)+	_ARA
+	bne.s	notara
+	cmp.w	#$0000,(a0)	version
+	bne.s	notara
+	
+	move.l	$f90006,-(sp)
 	pea	$1000000		located at 16 MB boundary
 	move.w	#20,-(sp)
 	trap	#1
 	lea	10(sp),sp
 	
-	move.w	#32,-(sp)
+notara	move.w	#32,-(sp)
 	trap	#1		go into User mode
 	addq	#6,sp
 
@@ -29,7 +35,7 @@ pryc	move.w	#32,-(sp)
 	trap	#1
 
 	section DATA
-infotext	dc.b	13,10,27,'p',"  ARAnyM TT-RAM 1.0 ",27,'q',13,10,10,0
+infotext	dc.b	13,10,27,'p',"  ARAnyM TT-RAM 1.1 ",27,'q',13,10,10,0
 
 	section BSS
 kilobyte	ds.l	256
