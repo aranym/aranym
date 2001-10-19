02016396ea54ffd52ded56356fedbf06966420e4
joy <>
2001-10-19 08:50:11 +0000

    version increased for the first public release

--- src/include/version.h
+++ src/include/version.h
@@ -23,8 +23,8 @@
 
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 0;
-const int VERSION_SUBMINOR = 8;
+const int VERSION_SUBMINOR = 9;
 
-#define VERSION_STRING "ARAnyM 0.0.8"
+#define VERSION_STRING "ARAnyM 0.0.9"
 
 #endif
