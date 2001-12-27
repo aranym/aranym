16f2d375792869c563afc4635fd449acbc903821
joy <>
2001-12-27 22:49:18 +0000

    version++

--- src/include/version.h
+++ src/include/version.h
@@ -23,8 +23,8 @@
 
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 0;
-const int VERSION_SUBMINOR = 14;
+const int VERSION_SUBMINOR = 15;
 
-#define VERSION_STRING "ARAnyM 0.0.14"
+#define VERSION_STRING "ARAnyM 0.0.15"
 
 #endif
