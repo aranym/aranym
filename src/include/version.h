1b3ddfea6de456b3c3977c96a004c57616f35e33
joy <>
2002-03-22 20:18:48 +0000

    fixed FPU is a great thing so I am increasing the micro version.

--- src/include/version.h
+++ src/include/version.h
@@ -23,8 +23,8 @@
 
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 1;
-const int VERSION_SUBMINOR = 0;
+const int VERSION_SUBMINOR = 1;
 
-#define VERSION_STRING "ARAnyM 0.1.0"
+#define VERSION_STRING "ARAnyM 0.1.1"
 
 #endif
