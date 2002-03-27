94fd8d764e6044336b88620d52fdcada55f531a8
joy <>
2002-03-27 23:00:49 +0000

    fVDI driver fixed so version increased.

--- src/include/version.h
+++ src/include/version.h
@@ -23,8 +23,8 @@
 
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 1;
-const int VERSION_SUBMINOR = 2;
+const int VERSION_SUBMINOR = 3;
 
-#define VERSION_STRING "ARAnyM 0.1.2"
+#define VERSION_STRING "ARAnyM 0.1.3"
 
 #endif
