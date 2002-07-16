52851199c3d0c0ce2924f2715aab6d95e4907229
pmandin <>
2002-07-16 13:42:13 +0000

    Host port stuff reworked

--- src/dsp.cpp
+++ src/dsp.cpp
@@ -1,4 +1,12 @@
-/* Joy 2001 */
+/*
+ * DSP56K emulation
+ * host port
+ *
+ * Joy 2001
+ * Patrice Mandin
+ */
+
+#include <math.h>
 
 #include "sysdeps.h"
 #include "hardware.h"
@@ -18,10 +26,114 @@ void DSP::init(void)
 	memset(periph, 0,sizeof(periph));
 	memset(stack, 0,sizeof(stack));
 	memset(registers, 0,sizeof(registers));
-	/* FIXME : initialize rom[] */
+
+	/* Initialize Y:rom[0x0100-0x01ff] with a sin table */
+	{
+		float src;
+		int32 dest;
+
+		for (i=0;i<256;i++) {
+			src = (((float) i)*M_PI)/128.0;
+			dest = (int32) (sin(src) * 8388608.0); /* 1<<23 */
+			if (dest>8388607) {
+				dest = 8388607;
+			} else if (dest<-8388608) {
+				dest = -8388608;
+			}
+			rom[SPACE_Y][0x100+i]=dest & 0x00ffffff;
+		}
+	}
+
+	/* FIXME: Initialize X:rom[0x0100-0x017f] with a mu-law table */
+	{
+/*		int j;*/
+			
+		for (i=0;i<128;i++) {
+			rom[SPACE_X][0x100+i]=0;
+		}
+/*
+		for (i=0;i<16;i++) {
+			rom[SPACE_X][0x100+i]=0x7c00+((0x7d-i*4)<<16);
+		}
+		for (i=0;i<16;i++) {
+			rom[SPACE_X][0x110+i]=0x7c00+((0x3e-i*2)<<16);
+		}
+		for (i=0;i<16;i++) {
+			rom[SPACE_X][0x120+i]=0xfc00+((0x1e-i)<<16);
+		}
+		for (i=0;i<16;i++) {
+			if (i & 1) {
+				j = 0xbc00;
+			} else {
+				j = 0x3c00;
+			}
+			rom[SPACE_X][0x130+i]=j;
+		}
+		for (i=0;i<16;i++) {
+			switch(i & 3) {
+				case 0:
+					j=0x5c00;
+					break;
+				case 1:
+					j=0x1c00;
+					break;
+				case 2:
+					j=0xdc00;
+					break;
+				case 3:
+					j=0x9c00;
+					break;
+			}
+			j += (7-(i>>2))<<16;
+			rom[SPACE_X][0x140+i]=j;
+		}
+
+		for (i=0;i<128;i++) {
+			D(bug("Dsp: 0x%02x: 0x%06x",i,rom[SPACE_X][0x100+i]));
+		}
+*/
+	}
+
+	/* Initialize X:rom[0x0180-0x01ff] with a a-law table */
+	{
+		const int32 multiply_base[8]={
+			0x1580, 0x0ac0, 0x5600, 0x2b00,
+			0x1580, 0x0058, 0x0560, 0x02b0
+		};
+		const int32 multiply_col[4]={0x10, 0x01, 0x04, 0x02};
+		const int32 multiply_line[4]={0x40, 0x04, 0x10, 0x08};
+		const int32 base_values[4]={0, -1, 2, 1};
+		uint32 pos=0x0180;
+		
+		for (i=0;i<8;i++) {
+			int32 alawbase, j;
+
+			alawbase = multiply_base[i]<<8;
+			for (j=0;j<4;j++) {
+				int32 alawbase1, k;
+				
+				alawbase1 = alawbase + ((base_values[j]*multiply_line[i & 3])<<12);
+
+				for (k=0;k<4;k++) {
+					int32 alawbase2;
+
+					alawbase2 = alawbase1 + ((base_values[k]*multiply_col[i & 3])<<12);
+
+					rom[SPACE_X][pos++]=alawbase2;
+				}
+			}
+		}
+	}
+	
+	D(bug("Dsp: power-on done"));
+	reset();
+}
+
+void DSP::reset(void)
+{
+	int i;
 
 	state = DSP_BOOTING;
-	D(bug("Dsp: booting"));
 	bootstrap_pos = bootstrap_accum = 0;
 	
 	/* Registers */
@@ -32,15 +144,23 @@ void DSP::init(void)
 	}
 
 	/* host port init, dsp side */
-	periph[SPACE_X][PERIPH_HOST_HSR]=(1<<HOST_HSR_HTDE);
+	periph[SPACE_X][DSP_HOST_HSR]=(1<<DSP_HOST_HSR_HTDE);
 
 	/* host port init, cpu side */
-	periph[SPACE_X][CPU_HOST_CVR]=0x12;
-	periph[SPACE_X][CPU_HOST_ISR]=(1<<HOST_ISR_TRDY)|(1<<HOST_ISR_TXDE);
-	periph[SPACE_X][CPU_HOST_IVR]=0x0f;
+	hostport[CPU_HOST_CVR]=0x12;
+	hostport[CPU_HOST_ISR]=(1<<CPU_HOST_ISR_TRDY)|(1<<CPU_HOST_ISR_TXDE);
+	hostport[CPU_HOST_IVR]=0x0f;
+
+	/* Other hardware registers */
+	periph[SPACE_X][DSP_IPR]=0;
+	periph[SPACE_X][DSP_BCR]=0xffff;
 
 	/* Misc */
 	loop_rep = 0;
+	last_loop_inst = 0;
+	first_host_write = 1;
+
+	D(bug("Dsp: reset done"));
 }
 
 uae_u8 DSP::handleRead(uaecptr addr)
@@ -51,38 +171,45 @@ uae_u8 DSP::handleRead(uaecptr addr)
 
 	switch(addr) {
 		case CPU_HOST_ICR:
-			value = periph[SPACE_X][CPU_HOST_ICR];
+			value = hostport[CPU_HOST_ICR];
 			break;
 		case CPU_HOST_CVR:
-			value = periph[SPACE_X][CPU_HOST_CVR];
+			value = hostport[CPU_HOST_CVR];
 			break;
 		case CPU_HOST_ISR:
-			value = periph[SPACE_X][CPU_HOST_ISR];
+			value = hostport[CPU_HOST_ISR];
 			break;
 		case CPU_HOST_IVR:
-			value = periph[SPACE_X][CPU_HOST_IVR];
+			value = hostport[CPU_HOST_IVR];
 			break;
 		case CPU_HOST_RX0:
 			value = 0;
 			break;
 		case CPU_HOST_RXH:
-			value = (periph[SPACE_X][PERIPH_HOST_HRX]>>16) & 255;
+			value = hostport[CPU_HOST_RXH];
 			break;
 		case CPU_HOST_RXM:
-			value = (periph[SPACE_X][PERIPH_HOST_HRX]>>8) & 255;
+			value = hostport[CPU_HOST_RXM];
 			break;
 		case CPU_HOST_RXL:
-			value = periph[SPACE_X][PERIPH_HOST_HRX] & 255;
-#if 0
-			/* Set HTDE bit to say that CPU has read */
-			periph[SPACE_X][PERIPH_HOST_HSR] |= 1<<HOST_HSR_HTDE;
-			/* Clear RXDF bit to say that CPU has read */
-			periph[SPACE_X][CPU_HOST_ISR] &= 0xff-(1<<HOST_ISR_RXDF);
-#endif
+			value = hostport[CPU_HOST_RXL];
+
+			/* Wake up DSP if it was waiting our read */
+			if (state==DSP_WAITHOSTREAD) {
+/*				D(bug("Dsp: state = DSP_RUNNING"));*/
+				state = DSP_RUNNING;
+			}
+
+			if (state!=DSP_BOOTING) {
+				/* Clear RXDF bit to say that CPU has read */
+				hostport[CPU_HOST_ISR] &= 0xff-(1<<CPU_HOST_ISR_RXDF);
+/*				D(bug("Dsp: (D->H): Host RXDF cleared"));*/
+			}
+
 			break;
 	}
 
-	D(bug("HWget_b(0x%08x)=0x%02x at 0x%08x", addr+HW_DSP, value, showPC()));
+/*	D(bug("HWget_b(0x%08x)=0x%02x at 0x%08x", addr+HW_DSP, value, showPC()));*/
 	return value;
 }
 
@@ -92,29 +219,66 @@ void DSP::handleWrite(uaecptr addr, uae_u8 value)
 
 	switch(addr) {
 		case CPU_HOST_ICR:
-			periph[SPACE_X][CPU_HOST_ICR]=value & 0xfb;
+			hostport[CPU_HOST_ICR]=value & 0xfb;
+			/* Set HF1 and HF0 accordingly on the host side */
+			periph[SPACE_X][DSP_HOST_HSR] &=
+					0xff-((1<<DSP_HOST_HSR_HF1)|(1<<DSP_HOST_HSR_HF0));
+			periph[SPACE_X][DSP_HOST_HSR] |=
+					hostport[CPU_HOST_ICR] & ((1<<DSP_HOST_HSR_HF1)|(1<<DSP_HOST_HSR_HF0));
 			break;
 		case CPU_HOST_CVR:
-			periph[SPACE_X][CPU_HOST_CVR]=value & 0x9f;
+			hostport[CPU_HOST_CVR]=value & 0x9f;
 			/* if bit 7=1, host command */
+			if (value & (1<<7)) {
+				D(bug("Dsp: Host command for DSP"));
+			}
 			break;
 		case CPU_HOST_ISR:
 			/* Read only */
 			break;
 		case CPU_HOST_IVR:
-			periph[SPACE_X][CPU_HOST_IVR]=value;
+			hostport[CPU_HOST_IVR]=value;
 			break;
 		case CPU_HOST_TX0:
-			bootstrap_accum = 0;
+			if (first_host_write) {
+				first_host_write = 0;
+				bootstrap_accum = 0;
+			}
 			break;
 		case CPU_HOST_TXH:
+			if (first_host_write) {
+				first_host_write = 0;
+				bootstrap_accum = 0;
+			}
+			hostport[CPU_HOST_TXH]=value;
 			bootstrap_accum |= value<<16;
 			break;
 		case CPU_HOST_TXM:
+			if (first_host_write) {
+				first_host_write = 0;
+				hostport[CPU_HOST_TXH]=value;	/* FIXME: is it correct ? */
+				bootstrap_accum = 0;
+			}
+			hostport[CPU_HOST_TXM]=value;
 			bootstrap_accum |= value<<8;
 			break;
 		case CPU_HOST_TXL:
+			if (first_host_write) {
+				first_host_write = 0;
+				hostport[CPU_HOST_TXH]=value;	/* FIXME: is it correct ? */
+				hostport[CPU_HOST_TXM]=value;	/* FIXME: is it correct ? */
+				bootstrap_accum = 0;
+			}
+			hostport[CPU_HOST_TXL]=value;
 			bootstrap_accum |= value;
+
+			first_host_write = 1;
+
+			if (state!=DSP_BOOTING) {
+				/* Clear TXDE to say that host has written */
+				hostport[CPU_HOST_ISR] &= 0xff-(1<<CPU_HOST_ISR_TXDE);
+/*				D(bug("Dsp: (H->D): Host TXDE cleared"));*/
+			}
 
 			switch(state) {
 				case DSP_BOOTING:
@@ -126,23 +290,13 @@ void DSP::handleWrite(uaecptr addr, uae_u8 value)
 					}		
 					bootstrap_accum = 0;
 					break;
-				case DSP_WAITHOST:
-					/* Wake up DSP if a waiting data arrived */
-				case DSP_RUNNING:
-					/* New data available on host port */
-				case DSP_WAITINTERRUPT:
-					/* Wake up if host port interrupt */
-					periph[SPACE_X][PERIPH_HOST_HTX]=bootstrap_accum;
+				case DSP_WAITHOSTWRITE:
+					/* Wake up DSP if it was waiting our write */
+/*					D(bug("Dsp: state = DSP_RUNNING"));*/
 					state = DSP_RUNNING;
-					D(bug("Writing 0x%06x on host port, from 0x%08x", bootstrap_accum, showPC()));
 					break;
 			}
-#if 0
-			/* Set HRDF bit to say that DSP can read */
-			periph[SPACE_X][PERIPH_HOST_HSR] |= 1<<HOST_HSR_HRDF;
-			/* Clear TXDE bit to say that CPU has written */
-			periph[SPACE_X][CPU_HOST_ISR] &= 0xff-(1<<HOST_ISR_TXDE);
-#endif
+
 			break;
 	}
 
--- src/include/dsp.h
+++ src/include/dsp.h
@@ -6,16 +6,17 @@
 #define DSP_RAMSIZE 32768
 
 /* Dsp State */
-#define DSP_BOOTING 0
-#define DSP_RUNNING 1
-#define DSP_WAITHOST 2
-#define DSP_WAITINTERRUPT 3
+#define DSP_BOOTING 0		/* Dsp loads bootstrap code */
+#define DSP_RUNNING 1		/* Execute instructions */
+#define DSP_WAITHOSTWRITE 2	/* Dsp waits for host to write data */
+#define DSP_WAITHOSTREAD 3	/* Dsp waits for host to read data */
+#define DSP_HALT 4
 
-/* Host port, CPU side */		/* Mapped to these reserved space */
-#define CPU_HOST_ICR	0x00	/* x:ffc0 */
-#define CPU_HOST_CVR	0x01	/* x:ffc1 */
-#define CPU_HOST_ISR	0x02	/* x:ffc2 */
-#define CPU_HOST_IVR	0x03	/* x:ffc3 */
+/* Host port, CPU side */
+#define CPU_HOST_ICR	0x00
+#define CPU_HOST_CVR	0x01
+#define CPU_HOST_ISR	0x02
+#define CPU_HOST_IVR	0x03
 #define CPU_HOST_RX0	0x04
 #define CPU_HOST_RXH	0x05
 #define CPU_HOST_RXM	0x06
@@ -25,47 +26,62 @@
 #define CPU_HOST_TXM	0x06
 #define CPU_HOST_TXL	0x07
 
-#define HOST_ICR_RREQ	0x00
-#define HOST_ICR_TREQ	0x01
-#define HOST_ICR_HF0	0x03
-#define HOST_ICR_HF1	0x04
-#define HOST_ICR_HM0	0x05
-#define HOST_ICR_HM1	0x06
-#define HOST_ICR_INIT	0x07
+#define CPU_HOST_ICR_RREQ	0x00
+#define CPU_HOST_ICR_TREQ	0x01
+#define CPU_HOST_ICR_HF0	0x03
+#define CPU_HOST_ICR_HF1	0x04
+#define CPU_HOST_ICR_HM0	0x05
+#define CPU_HOST_ICR_HM1	0x06
+#define CPU_HOST_ICR_INIT	0x07
 
-#define HOST_CVR_HC		0x07
+#define CPU_HOST_CVR_HC		0x07
 
-#define HOST_ISR_RXDF	0x00
-#define HOST_ISR_TXDE	0x01
-#define HOST_ISR_TRDY	0x02
-#define HOST_ISR_HF2	0x03
-#define HOST_ISR_HF3	0x04
-#define HOST_ISR_DMA	0x06
-#define HOST_ISR_HREQ	0x07
+#define CPU_HOST_ISR_RXDF	0x00
+#define CPU_HOST_ISR_TXDE	0x01
+#define CPU_HOST_ISR_TRDY	0x02
+#define CPU_HOST_ISR_HF2	0x03
+#define CPU_HOST_ISR_HF3	0x04
+#define CPU_HOST_ISR_DMA	0x06
+#define CPU_HOST_ISR_HREQ	0x07
 
-/* Host port, DSP side */
-#define PERIPH_HOST_HCR 0x28	/* x:ffe8 */
-#define PERIPH_HOST_HSR 0x29	/* x:ffe9 */
-#define PERIPH_HOST_HRX 0x2b	/* x:ffeb */
-#define PERIPH_HOST_HTX 0x2b	/* x:ffeb */
+/* Host port, DSP side, DSP addresses are 0xffc0+value */
+#define DSP_PBC			0x20	/* Port B control register */
+#define DSP_PCC			0x21	/* Port C control register */
+#define DSP_PBDDR		0x22	/* Port B data direction register */
+#define DSP_PCDDR		0x23	/* Port C data direction register */
+#define DSP_PBD			0x24	/* Port B data register */
+#define DSP_PCD			0x25	/* Port C data register */
+#define DSP_HOST_HCR	0x28	/* Host control register */
+#define DSP_HOST_HSR	0x29	/* Host status register */
+#define DSP_HOST_HRX	0x2b	/* Host receive register */
+#define DSP_HOST_HTX	0x2b	/* Host transmit register */
+#define DSP_SSI_CRA		0x2c	/* Ssi control register A */
+#define DSP_SSI_CRB		0x2d	/* Ssi control register B */
+#define DSP_SSI_SR		0x2e	/* Ssi status register */
+#define DSP_SSI_TSR		0x2e	/* Ssi time slot register */
+#define DSP_SSI_RX		0x2f	/* Ssi receive register */
+#define DSP_SSI_TX		0x2f	/* Ssi transmit register */
+#define DSP_BCR			0x3e	/* Port A bus control register */
+#define DSP_IPR			0x3f	/* Interrupt priority register */
 
-#define HOST_HCR_HRIE	0x00
-#define HOST_HCR_HTIE	0x01
-#define HOST_HCR_HCIE	0x02
-#define HOST_HCR_HF2	0x03
-#define HOST_HCR_HF3	0x04
+#define DSP_HOST_HCR_HRIE	0x00
+#define DSP_HOST_HCR_HTIE	0x01
+#define DSP_HOST_HCR_HCIE	0x02
+#define DSP_HOST_HCR_HF2	0x03
+#define DSP_HOST_HCR_HF3	0x04
 
-#define HOST_HSR_HRDF	0x00
-#define HOST_HSR_HTDE	0x01
-#define HOST_HSR_HCP	0x02
-#define HOST_HSR_HF0	0x03
-#define HOST_HSR_HF1	0x04
-#define HOST_HSR_DMA	0x07
+#define DSP_HOST_HSR_HRDF	0x00
+#define DSP_HOST_HSR_HTDE	0x01
+#define DSP_HOST_HSR_HCP	0x02
+#define DSP_HOST_HSR_HF0	0x03
+#define DSP_HOST_HSR_HF1	0x04
+#define DSP_HOST_HSR_DMA	0x07
 
 class DSP : public BASE_IO {
 	
 	public:
 		void init(void);
+		void reset(void);
 		virtual uae_u8 handleRead(uaecptr addr);
 		virtual void handleWrite(uaecptr, uae_u8);
 
@@ -88,8 +104,13 @@ class DSP : public BASE_IO {
 		/* peripheral space, [x|y]:0xffc0-0xffff */
 		uint32	periph[2][64];
 
+		/* host port, CPU side */
+		uint8 hostport[8];
+
 		/* Misc */
-		uint32 loop_rep;	/* executing rep ? */
+		uint32 loop_rep;		/* executing rep ? */
+		uint32 last_loop_inst;	/* executing the last instruction in DO ? */
+		uint32 first_host_write;	/* first byte written to host port */
 
 	private:
 		/* For bootstrap routine */
