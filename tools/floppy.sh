79a0c0df62ca9de89a1c263f89aac1b403082740
milan <>
2002-07-20 10:01:14 +0000

    updated for new utilities

--- tools/floppy.sh
+++ tools/floppy.sh
@@ -10,6 +10,7 @@ mformat -C -f 1440 -v ARAFLOPPY t:
 
 mmd t:auto
 mmd t:gemsys
+mmd t:tools
 
 mcopy aratools/conxboot.prg t:auto/
 mcopy aratools/cecile.prg t:auto/
@@ -17,12 +18,16 @@ mcopy aratools/fvdi.prg t:auto/
 mcopy aratools/betados.prg t:auto/
 mcopy aratools/fastram.prg t:auto/
 mcopy aratools/clocky.prg t:auto/
+mcopy aratools/zmagxsnd.prg t:auto/
 mcopy aratools/bdconfig.sys t:auto/
 mcopy aratools/aranymfs.dos t:auto/
 
 mcopy aratools/aranym.sys t:gemsys/
 
 mcopy aratools/newdesk.inf t:
+
+mcopy aratools/fvdicout.app t:tools/
+mcopy aratools/pcpatch.prg t:tools/
 
 rm -rf ./aratools
 rm -f ./mtoolsrc
