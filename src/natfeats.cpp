482ccdd9b0893a5725a5642084b619daf820c468
joy <>
2002-08-04 21:04:33 +0000

    minor updates: NF_NAME and NF_VERSION defined

--- src/natfeats.cpp
+++ src/natfeats.cpp
@@ -16,40 +16,12 @@
 
 #define ID_SHIFT	20
 
-enum { NF_NAME = 1, NF_XHDI, NF_FVDI, NF_LAST };
+enum { NF_NAME = 1, NF_VERSION, NF_XHDI, NF_FVDI, NF_LAST };
 
-uint32 identify_dispatch_params(uint32 *params)
-{
-	uint32 fncode = params[0];
-	memptr name_ptr = params[1];
-	uint32 name_maxlen = params[2];
-
-	if (name_ptr < 0 || name_ptr > (RAMSize + ROMSize + FastRAMSize - name_maxlen))
-		return 0;	/* illegal pointer to name */
-	char *name = (char *)Atari2HostAddr(name_ptr);
-
-	uint32 ret = 0;
-	switch(fncode) {
-		case 0:	/* get_name_and_version(char *name, uint32 max_len) */
-			strncpy(name, VERSION_STRING, name_maxlen-1);
-			name[name_maxlen-1] = '\0';
-			break;
-
-		case 1:	/* get_name(char *name, uint32 max_len) */
-			strncpy(name, "ARAnyM", name_maxlen-1);
-			name[name_maxlen-1] = '\0';
-			break;
-
-		case 2:	/* get_version_num(void) */
-			ret = VERSION_MAJOR << 16 | VERSION_MINOR;
-			break;
-
-		default:
-			ret = 1;	/* FIXME illegal subfunc ID */
-			break;
-	}
-	return ret;
-}
+/* forward declaration until these functions find a new home in
+   a separate file */
+uint32 nf_name(uint32 *params);
+uint32 nf_version(uint32 *params);
 
 uint32 nf_get_id(memptr stack)
 {
@@ -57,7 +29,8 @@ uint32 nf_get_id(memptr stack)
 
 	typedef struct NATFEATS { const char *name; int id; };
 	NATFEATS nat_feats[] = {
-		{"NAME", NF_NAME},
+		{"NF_NAME", NF_NAME},
+		{"NF_VERSION", NF_VERSION},
 		{"XHDI", NF_XHDI},
 		{"fVDI", NF_FVDI},
 		{NULL, NF_LAST}
@@ -92,12 +65,44 @@ uint32 nf_rcall(memptr stack)
 		parameters[i] = ReadInt32(stack + i * 4);
 
 	switch(fncode >> ID_SHIFT) {
-		case NF_NAME: return identify_dispatch_params(parameters); break;
+		case NF_NAME: return nf_name(parameters); break;
+		case NF_VERSION: return nf_version(parameters); break;
 
-		case NF_XHDI: return Xhdi.dispatch_params(parameters); break;
+		case NF_XHDI: return Xhdi.dispatch(parameters); break;
 
 		// case NF_FVDI: return fVDI.dispatch_params(parameters); break;
 
 		default: return 0;	/* FIXME: is this a good answer for wrong ID call? */
 	}
+}
+
+/* Basic NF */
+uint32 nf_name(uint32 *params)
+{
+	uint32 fncode = params[0];
+	memptr name_ptr = params[1];
+	uint32 name_maxlen = params[2];
+
+	if (name_ptr < 0 || name_ptr > (RAMSize + ROMSize + FastRAMSize - name_maxlen))
+		return 0;	/* illegal pointer to name */
+	char *name = (char *)Atari2HostAddr(name_ptr);
+
+	uint32 ret = 0;
+	switch(fncode) {
+		default:/* get_pure_name(char *name, uint32 max_len) */
+			strncpy(name, "ARAnyM", name_maxlen-1);
+			name[name_maxlen-1] = '\0';
+			break;
+
+		case 1:	/* get_complete_name(char *name, uint32 max_len) */
+			strncpy(name, VERSION_STRING, name_maxlen-1);
+			name[name_maxlen-1] = '\0';
+			break;
+	}
+	return ret;
+}
+
+uint32 nf_version(uint32 *params)
+{
+	return VERSION_MAJOR << 16 | VERSION_MINOR;
 }
