c2a582bb06582b20276d650d88910ad022e8dc3f
joy <>
2002-08-11 08:35:04 +0000

    new revision of NatFeats

---
+++ src/include/natfeats.h
@@ -0,0 +1,13 @@
+#ifndef _NATFEATS_H
+#define _NATFEATS_H
+
+#include "sysdeps.h"
+
+// NatFeats CPU handlers
+extern uint32 nf_get_id(memptr);
+extern uint32 nf_rcall(memptr, bool);
+
+// NatFeats call for getting parameters
+extern uint32 nf_getparameter(int);
+
+#endif /* _NATFEATS_H */
