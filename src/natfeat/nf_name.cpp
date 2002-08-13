fd95cab3a42a7d87c390dd9cc6bd8b7135ec2964
joy <>
2002-08-13 08:10:04 +0000

    NF_Name updated

--- src/natfeat/nf_name.cpp
+++ src/natfeat/nf_name.cpp
@@ -23,16 +23,18 @@ int32 NF_Name::dispatch(uint32 fncode)
 
 	char *name = (char *)Atari2HostAddr(name_ptr);	// use A2Hstrcpy
 
-	uint32 ret = 0;
+	uint32 ret;
 	switch(fncode) {
 		default:/* get_pure_name(char *name, uint32 max_len) */
 			strncpy(name, NAME_STRING, name_maxlen-1);
 			name[name_maxlen-1] = '\0';
+			ret = sizeof(NAME_STRING);
 			break;
 
 		case 1:	/* get_complete_name(char *name, uint32 max_len) */
 			strncpy(name, VERSION_STRING, name_maxlen-1);
 			name[name_maxlen-1] = '\0';
+			ret = sizeof(VERSION_STRING);
 			break;
 	}
 	return ret;
