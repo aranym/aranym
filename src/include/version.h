cbf622bd19a86ed52fefb683a1710ce55a23d486
joy <>
2002-10-11 12:12:09 +0000

    ver++

--- src/include/version.h
+++ src/include/version.h
@@ -23,9 +23,9 @@
 
 const int VERSION_MAJOR = 0;
 const int VERSION_MINOR = 6;
-const int VERSION_SUBMINOR = 5;
+const int VERSION_SUBMINOR = 6;
 
 #define NAME_STRING "ARAnyM"
-#define VERSION_STRING NAME_STRING" 0.6.5-beta"
+#define VERSION_STRING NAME_STRING" 0.6.6-beta"
 
 #endif
