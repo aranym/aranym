7253cbd78551a06acbec92ceb46b600642951f35
standa <>
2003-02-12 19:37:34 +0000

    The bash -> sh conversion Added an option to not to write the MBR (create an empty file) tested on /bin/sh from:   SunOS 5.8 Generic_108528-07 sun4u sparc SUNW,Ultra-80   FreeBSD 4.7-STABLE FreeBSD 4.7-STABLE   HP-UX hp B.11.11 U 9000/800

--- tools/createdisk/createdisk.sh
+++ tools/createdisk/createdisk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # ***********************************
 # * Plex86 Empty Disk Image Utility *
@@ -10,20 +10,18 @@
 # *          v. 20001125            *
 # ***********************************
 
-MBR_DATA="mbrdata"
-
 # *** Standard CHS, limit 504MB ***
 
-let "MAX_C = 1024"
-let "MAX_H = 16"
-let "MAX_S = 63"
+MAX_C=1024
+MAX_H=16
+MAX_S=63
 
-let "B_PER_S = 512"
-let "MAX_SIZE = $MAX_C * $MAX_H * $MAX_S * $B_PER_S"
+B_PER_S=512
+MAX_SIZE=`expr \( $MAX_C \* $MAX_H \* $MAX_S \* $B_PER_S \) / 1024`
 
 # $1 : File, $2 : Size
 
-if [ -z $1 ] || [ -z $2 ] || [ `echo $2 | grep [^0-9] | wc -l` -gt 0 ]
+if [ -z "$1" ] || [ -z "$2" ] || [ `echo $2 | grep [\^0-9] | wc -l` -gt 0 ]
 then
   echo "Usage:"
   echo "$0 file size"
@@ -32,35 +30,54 @@ then
   exit 1
 fi
 
-let "REQ_SIZE = $2 * 1024 * 1024"
+if [ -n "$3" ]
+then
+  MBR_DATA="$3"
+else
+  MBR_DATA="mbrdata"
+fi
 
-if [ $REQ_SIZE -gt $MAX_SIZE ]
+REQ_SIZE=`expr $2 \* 1024`
+
+if [ $REQ_SIZE -gt $MAX_SIZE ] && [ -f "$MBR_DATA" ]
 then
   echo "Requested size is too big!"
+  echo "You might want to create an empty image file without MBR (see the README)."
   exit -2
 fi
 
-let "SIZE = $REQ_SIZE / $B_PER_S"
-let "C = $SIZE / ($MAX_H * $MAX_S)"
-let "H = $MAX_H"
-let "S = $MAX_S"
-let "BLOCKS = $C * $H * $S"
-let "SIZE = $BLOCKS * $B_PER_S"
+SIZE=`expr \( $REQ_SIZE / $B_PER_S \) \* 1024`
+C=`expr $SIZE / \( $MAX_H \* $MAX_S \)`
+H=`expr $MAX_H`
+S=`expr $MAX_S`
+BLOCKS=`expr $C \* $H \* $S`
+SIZE=`expr $BLOCKS \* $B_PER_S`
 
-echo "Disk Geometry:"
-echo "C: $C"
-echo "H: $H"
-echo "S: $S"
-echo "Total size: $SIZE bytes"
+SEEKBLOCKS=`expr $BLOCKS \- 1`
+dd if=/dev/zero of=$1 bs=$B_PER_S seek=$SEEKBLOCKS count=1 2> /dev/null
 
-dd if=/dev/zero of=$1 bs=$B_PER_S count=$BLOCKS 2> /dev/null
+echo "# Disk Geometry:"
+echo "# C: $C"
+echo "# H: $H"
+echo "# S: $S"
+echo "# Total size: $SIZE bytes"
 
-if [ -e $MBR_DATA ]
+if [ -f "$MBR_DATA" ]
 then
   dd if=$MBR_DATA of=$1 bs=$B_PER_S count=1 conv=notrunc 2> /dev/null
-  echo "Wrote master boot record from file $MBR_DATA"
+  echo "# Wrote master boot record from file $MBR_DATA"
 fi
+echo
 
+echo "# You should add the following lines to your ARAnyM config:"
+echo "[ID0]"
+echo " Present = Yes"
+echo " IsCDROM = No"
+echo " Path = $1"
+echo " Cylinders = $C" 
+echo " Heads = $H"
+echo " SectorsPerTrack = $S" 
+echo " ByteSwap = No"
+  
 exit 0
-
 
