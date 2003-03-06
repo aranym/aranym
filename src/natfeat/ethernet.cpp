86b4c97c12bb40ea4aa2d4ccebe5c925e0b82a31
joy <>
2003-03-06 16:31:44 +0000

    external suid configuration

--- src/natfeat/ethernet.cpp
+++ src/natfeat/ethernet.cpp
@@ -8,13 +8,12 @@
 #include "main.h"
 #include "ethernet.h"
 
-#include "host.h"
-
 #define DEBUG 0
 #include "debug.h"
 
 #include <sys/poll.h>
 #include <sys/socket.h>
+#include <sys/wait.h>	// waitpid()
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <linux/if.h>
@@ -32,17 +31,21 @@
  * Configuration zone begins
  */
 
+#define TAP_INIT	"aratapif"
+#define TAP_DEVICE	"tap0"
+#define TAP_MTU		"1500"
+
 #if !defined(XIF_HOST_IP) && !defined(XIF_ATARI_IP) && !defined(XIF_NETMASK)
 # define XIF_HOST_IP	"192.168.0.1"
 # define XIF_ATARI_IP	"192.168.0.2"
 # define XIF_NETMASK	"255.255.255.0"
 #endif
 
-// the ETHERNET NatFeat API version number (change it if you change NF subIDs)
-#define XIF_VERSION	0x00000002
-
 // the emulated network card HW address
 #define MAC_ADDRESS "\001\002\003\004\005\006"
+
+// the ETHERNET NatFeat API version number (change it if you change NF subIDs)
+#define XIF_VERSION	0x00000002
 
 // Ethernet runs at interrupt level 3 by default but can be reconfigured
 #if 1
@@ -168,7 +171,6 @@ void ETHERNETDriver::finishInterupt()
 	SDL_SemPost(intAck);
 }
 
-int initTap (int argc, char **argv);
 
 /*
  *  Initialization
@@ -180,30 +182,42 @@ bool ETHERNETDriver::init(void)
 
 	D(bug("Ethernet: init"));
 
-/* argv[0]      Name of this program                                 */
-/* argv[1]      Name of the TUN network device (tun0)                */
-/* argv[2]      The maximum transmission unit size                   */
-/* argv[3]      The IP address of the Atari side of the link         */
-/* argv[4]      The IP address of the host system side of the link   */
-/* argv[5]      The netmask                                          */
-	char *argv[] = {
-		"aranym",
-		"tap0",
-		"1500",
-		XIF_ATARI_IP,
-		XIF_HOST_IP,
-		XIF_NETMASK
-	};
-
 	fd = tapOpen( devName );
 	if (fd < 0) {
 		panicbug("NO_NET_DRIVER_WARN '%s': %s", devName, strerror(errno));
 		return false;
 	}
 
-	if ( initTap( 6, argv ) ) {
-		panicbug("initTap failure");
-		return false;
+	int pid = fork();
+	if (pid == 0) {
+		int result;
+		if ( (result = execlp( TAP_INIT, TAP_INIT, TAP_DEVICE, TAP_MTU,
+							XIF_ATARI_IP, XIF_HOST_IP, XIF_NETMASK) )) {
+			panicbug("initTap failure: %d", result);
+			if (result == -1) {
+				perror("execl tunifc");
+				result = 5;
+			}
+		}
+		::exit(result);
+	}
+	D(bug("waiting for tunifc"));
+	int status;
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status)) {
+		if (WEXITSTATUS(status)) {
+			D(bug("tunifc failed, closing device"));
+			// Close /dev/net/tun device
+			if (fd > 0)
+				close(fd);
+			return false;
+		}
+		else {
+			D(bug("tunifc initialized OK"));
+		}
+	}
+	else {
+		D(bug("tunifc failed badly"));
 	}
 
 	// Set nonblocking I/O
@@ -384,222 +398,3 @@ int ETHERNETDriver::tapOpen(char *dev)
 }
 
 #endif /* New driver support */
-
-
-
-
-/* HERCIFC.C    (c) Copyright Roger Bowler, 2000-2001                */
-/*              Hercules interface configuration program             */
-
-/*-------------------------------------------------------------------*/
-/* This module configures the TUN interface for Hercules.            */
-/* It is invoked as a setuid root program by ctcadpt.c               */
-/* The command line arguments are:                                   */
-/* argv[0]      Name of this program                                 */
-/* argv[1]      Name of the TUN network device (tun0)                */
-/* argv[2]      The maximum transmission unit size                   */
-/* argv[3]      The IP address of the Hercules side of the link      */
-/* argv[4]      The IP address of the driving system side of the link*/
-/* argv[5]      The netmask                                          */
-/* Error messages are written to stderr, which is redirected to      */
-/* the Hercules message log by ctcadpt.c                             */
-/* The exit status is zero if successful, non-zero if error.         */
-/*-------------------------------------------------------------------*/
-
-//#include "hercules.h"
-
-/*-------------------------------------------------------------------*/
-/* Subroutine to configure the MTU size into the interface           */
-/*-------------------------------------------------------------------*/
-static int
-set_mtu (int sockfd, char *ifname, char *mtusize)
-{
-int             rc;                     /* Return code               */
-struct ifreq    ifreq;                  /* Structure for ioctl       */
-
-    /* Initialize the structure for ioctl */
-    memset (&ifreq, 0, sizeof(ifreq));
-    strcpy (ifreq.ifr_name, ifname);
-    ifreq.ifr_mtu = atoi(mtusize);
-
-    /* Set the MTU size */
-    rc = ioctl (sockfd, SIOCSIFMTU, &ifreq);
-    if (rc < 0)
-    {
-        fprintf (stderr,
-            "HHC894I Error setting MTU for %s: %s\n",
-            ifname, strerror(errno));
-        return 1;
-    }
-
-    return 0;
-} /* end function set_mtu */
-
-/*-------------------------------------------------------------------*/
-/* Subroutine to configure an IP address into the interface          */
-/*-------------------------------------------------------------------*/
-static int
-set_ipaddr (int sockfd, char *ifname, int oper,
-            char *opname, char *ipaddr)
-{
-int             rc;                     /* Return code               */
-struct ifreq    ifreq;                  /* Structure for ioctl       */
-struct sockaddr_in *sin;                /* -> sockaddr within ifreq  */
-
-    /* Initialize the structure for ioctl */
-    memset (&ifreq, 0, sizeof(ifreq));
-    strcpy (ifreq.ifr_name, ifname);
-
-    /* Determine where in the structure to store the address */
-    sin = (struct sockaddr_in*)
-            (oper == SIOCSIFADDR ? &ifreq.ifr_addr
-            :oper == SIOCSIFDSTADDR ? &ifreq.ifr_dstaddr
-            :oper == SIOCSIFNETMASK ? &ifreq.ifr_netmask
-            :NULL);
-
-    /* Store the IP address into the structure */
-    sin->sin_family = AF_INET;
-    rc = inet_aton (ipaddr, &sin->sin_addr);
-    if (rc == 0)
-    {
-        fprintf (stderr,
-            "HHC896I Invalid %s for %s: %s\n",
-            opname, ifname, ipaddr);
-        return 1;
-    }
-
-    /* Configure the interface with the specified IP address */
-    rc = ioctl (sockfd, oper, &ifreq);
-    if (rc < 0)
-    {
-        fprintf (stderr,
-            "HHC897I Error setting %s for %s: %s\n",
-            opname, ifname, strerror(errno));
-        return 1;
-    }
-
-    return 0;
-} /* end function set_ipaddr */
-
-/*-------------------------------------------------------------------*/
-/* Subroutine to set the interface flags                             */
-/*-------------------------------------------------------------------*/
-static int
-set_flags (int sockfd, char *ifname, int flags)
-{
-int             rc;                     /* Return code               */
-struct ifreq    ifreq;                  /* Structure for ioctl       */
-
-    /* Initialize the structure for ioctl */
-    memset (&ifreq, 0, sizeof(ifreq));
-    strcpy (ifreq.ifr_name, ifname);
-
-    /* Get the current flags */
-    rc = ioctl (sockfd, SIOCGIFFLAGS, &ifreq);
-    if (rc < 0)
-    {
-        fprintf (stderr,
-            "HHC898I Error getting flags for %s: %s\n",
-            ifname, strerror(errno));
-        return 1;
-    }
-
-    /* Set the flags */
-    ifreq.ifr_flags |= flags;
-    rc = ioctl (sockfd, SIOCSIFFLAGS, &ifreq);
-    if (rc < 0)
-    {
-        fprintf (stderr,
-            "HHC899I Error setting flags for %s: %s\n",
-            ifname, strerror(errno));
-        return 1;
-    }
-
-    return 0;
-} /* end function set_flags */
-
-/*-------------------------------------------------------------------*/
-/* HERCIFC program entry point                                       */
-/*-------------------------------------------------------------------*/
-int initTap (int argc, char **argv)
-{
-char           *progname;               /* Name of this program      */
-char           *tundevn;                /* Name of TUN device        */
-char           *mtusize;                /* MTU size (characters)     */
-char           *hercaddr;               /* Hercules IP address       */
-char           *drivaddr;               /* Driving system IP address */
-char           *netmask;                /* Network mask              */
-int             sockfd;                 /* Socket descriptor         */
-int             errflag = 0;            /* 1=error(s) occurred       */
-char            ifname[IFNAMSIZ];       /* Interface name: tun[0-9]  */
-
-    /* Check for correct number of arguments */
-    if (argc != 6)
-    {
-        fprintf (stderr,
-            "HHC891I Incorrect number of arguments passed to %s\n",
-            argv[0]);
-        exit(1);
-    }
-
-    /* Copy the argument pointers */
-    progname = argv[0];
-    tundevn = argv[1];
-    mtusize = argv[2];
-    hercaddr = argv[3];
-    drivaddr = argv[4];
-    netmask = argv[5];
-
-    /* Only check for length, since network device-names can actually
-     * be anything!!
-     */
-    if (strlen(tundevn) > (IFNAMSIZ - 1))
-    {
-        fprintf (stderr,
-            "HHC892I Invalid device name %s passed to %s\n",
-            tundevn, progname);
-        exit(2);
-    }
-    strcpy(ifname, tundevn);
-
-    /* Obtain a socket for ioctl operations */
-    sockfd = socket (AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0)
-    {
-        fprintf (stderr,
-            "HHC893I %s cannot obtain socket: %s\n",
-            progname, strerror(errno));
-        exit(3);
-    }
-
-    /* Set the MTU size */
-    if (set_mtu (sockfd, ifname, mtusize))
-        errflag = 1;
-
-    /* Set the driving system's IP address */
-    if (set_ipaddr (sockfd, ifname, SIOCSIFADDR,
-        "driving system IP addr", drivaddr))
-        errflag = 1;
-
-    /* Set the Hercules IP address */
-    if (set_ipaddr (sockfd, ifname, SIOCSIFDSTADDR,
-        "Hercules IP addr", hercaddr))
-        errflag = 1;
-
-    /* Set the netmask */
-    if (set_ipaddr (sockfd, ifname, SIOCSIFNETMASK,
-        "netmask", netmask))
-        errflag = 1;
-
-    /* Set the flags */
-    if (set_flags (sockfd, ifname,
-                IFF_UP | IFF_RUNNING | IFF_POINTOPOINT ))
-        errflag = 1;
-
-    /* Exit with status code 4 if any errors occurred */
-    if (errflag) return 4;
-
-    /* Exit with status code 0 if no errors occurred */
-    return 0;
-
-} /* end function main */
