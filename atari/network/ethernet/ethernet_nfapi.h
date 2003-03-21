69787464439032cfccc3f2ab891c6fb0fab8b176
joy <>
2003-03-21 22:58:06 +0000

    better description

--- atari/network/ethernet/ethernet_nfapi.h
+++ atari/network/ethernet/ethernet_nfapi.h
@@ -12,13 +12,22 @@
 /* if you change anything in the enum {} below you have to increase 
    this ARAETHER_NFAPI_VERSION!
 */
-#define ARAETHER_NFAPI_VERSION	0x00000003
+#define ARAETHER_NFAPI_VERSION	0x00000004
 
 enum {
-	GET_VERSION = 0,	/* subID = 0 */
-	XIF_INTLEVEL, XIF_GETMAC, XIF_IRQ, XIF_START, XIF_STOP, XIF_READLENGTH,
-	XIF_READBLOCK, XIF_WRITEBLOCK,
-	XIF_GET_IPHOST, XIF_GET_IPATARI, XIF_GET_NETMASK
+	GET_VERSION = 0,	/* no parameters, return NFAPI_VERSION in d0 */
+	XIF_INTLEVEL,		/* no parameters, return Interrupt Level in d0 */
+	XIF_IRQ,			/* (ethX), invokes interrupt on Atari(?) */
+	XIF_START,			/* (ethX), called on 'ifup', starts receiver thread */
+	XIF_STOP,			/* (ethX), called on 'ifdown', stops the thread */
+	XIF_READLENGTH,		/* (ethX), return size of network data block to read */
+	XIF_READBLOCK,		/* (ethX, buffer, size), read block of network data */
+	XIF_WRITEBLOCK,		/* (ethX, buffer, size), writes block of network data */
+	XIF_GET_MAC,		/* (ethX, buffer, size), return MAC HW addr in buffer */
+	XIF_GET_IPHOST,		/* (ethX, buffer, size), return IP address of host */
+	XIF_GET_IPATARI,	/* (ethX, buffer, size), return IP address of atari */
+	XIF_GET_NETMASK,	/* (ethX, buffer, size), return IP netmask */
+	XIF_GET_MTU			/* (ethX), return MTU size in bytes in d0 */
 };
 
 #define ETH(a)	(nfEtherFsId + a)
