27877ece99edd0763ddcdf57dd0d7e8aa34bba57
joy <>
2003-03-21 23:25:50 +0000

    slightly corrected

--- atari/network/ethernet/ethernet_nfapi.h
+++ atari/network/ethernet/ethernet_nfapi.h
@@ -17,12 +17,12 @@
 enum {
 	GET_VERSION = 0,	/* no parameters, return NFAPI_VERSION in d0 */
 	XIF_INTLEVEL,		/* no parameters, return Interrupt Level in d0 */
-	XIF_IRQ,			/* (ethX), invokes interrupt on Atari(?) */
-	XIF_START,			/* (ethX), called on 'ifup', starts receiver thread */
-	XIF_STOP,			/* (ethX), called on 'ifdown', stops the thread */
+	XIF_IRQ,			/* (ethX), acknowledge interrupt from host */
+	XIF_START,			/* (ethX), called on 'ifup', start receiver thread */
+	XIF_STOP,			/* (ethX), called on 'ifdown', stop the thread */
 	XIF_READLENGTH,		/* (ethX), return size of network data block to read */
 	XIF_READBLOCK,		/* (ethX, buffer, size), read block of network data */
-	XIF_WRITEBLOCK,		/* (ethX, buffer, size), writes block of network data */
+	XIF_WRITEBLOCK,		/* (ethX, buffer, size), write block of network data */
 	XIF_GET_MAC,		/* (ethX, buffer, size), return MAC HW addr in buffer */
 	XIF_GET_IPHOST,		/* (ethX, buffer, size), return IP address of host */
 	XIF_GET_IPATARI,	/* (ethX, buffer, size), return IP address of atari */
