0fe970cbe5a3fbefc8e2e4315e4c62e6f0082a99
joy <>
2003-03-24 08:34:01 +0000

    dotted bin renamed

--- atari/network/ethernet/make.sh
+++ atari/network/ethernet/make.sh
@@ -15,9 +15,9 @@ cp -r ../ethernet $ETHERNET_BUILD_HOME
 # build them (all or debug targets are available)
 (cd $ETHERNET_BUILD_HOME/ethernet; make distclean; make $@)
 
-# move the binaries into the .bin folder
-mkdir -p .bin
-cp $ETHERNET_BUILD_HOME/ethernet/*.xif .bin
+# move the binaries into the bin folder
+mkdir -p bin
+cp $ETHERNET_BUILD_HOME/ethernet/*.xif bin
 
 # cleanup the FreeMiNT cvs tree
 rm -rf $ETHERNET_BUILD_HOME/ethernet
