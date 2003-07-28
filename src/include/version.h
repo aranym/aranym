8dd3ab20f6a3b505935582d5677cf8cee82e749b
joy <>
2003-07-28 20:44:18 +0000

    ver++

--- src/include/version.h
+++ src/include/version.h
@@ -23,11 +23,11 @@
 
 #define VER_MAJOR	0
 #define VER_MINOR	8
-#define VER_MICRO	0
+#define VER_MICRO	1
 
 #ifndef VER_STATUS
-//#define VER_STATUS	"+CVS " __DATE__
-#define VER_STATUS	"beta3"
+#define VER_STATUS	"+CVS " __DATE__
+//#define VER_STATUS	"beta1"
 #endif
 
 #define str(x)		_stringify (x)
