a307033438292e9ab063dd058e0f4798b6d02d88
joy <>
2003-10-04 22:03:12 +0000

    ver++ again

--- src/include/version.h
+++ src/include/version.h
@@ -26,8 +26,8 @@
 #define VER_MICRO	2
 
 #ifndef VER_STATUS
-#define VER_STATUS	"alpha"
-//#define VER_STATUS	"beta"
+//#define VER_STATUS	"alpha"
+#define VER_STATUS	"beta"
 #endif
 
 #define str(x)		_stringify (x)
