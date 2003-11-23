d60a8bc790c0d2bc4902e667b004a1dd406293c5
stefanq <>
2003-11-23 16:45:41 +0000

    Added source files for Mac OS X/darwin.

---
+++ src/Unix/darwin/SDLMain.h
@@ -0,0 +1,18 @@
+/*   SDLMain.m - main entry point for our Cocoa-ized SDL app
+       Initial Version: Darrell Walisser <dwaliss1@purdue.edu>
+       Non-NIB-Code & other changes: Max Horn <max@quendi.de>
+
+    Feel free to customize this file to suit your needs
+*/
+
+#import <Cocoa/Cocoa.h>
+
+@interface SDLMain : NSObject
+{
+}
+
+- (IBAction) quit:(id)obj;
+- (IBAction) showPrefs:(id)obj;
+- (IBAction) makeFullscreen:(id)obj;
+
+@end
