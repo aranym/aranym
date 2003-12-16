575d8d87471b069457f9d306f54d0704745d7566
joy <>
2003-12-16 20:18:40 +0000

    corrected copy&paste error

--- src/natfeat/hostfs.cpp
+++ src/natfeat/hostfs.cpp
@@ -17,9 +17,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Atari800; if not, write to the Free Software
+ * along with ARAnyM; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ */
 
 #include "sysdeps.h"
 
