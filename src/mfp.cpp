119b8f25273c90f3a0eaa77998977263d53ac495
joy <>
2003-12-16 20:15:56 +0000

    initializing local variables is necessary when one wants the aranym to boot

--- src/mfp.cpp
+++ src/mfp.cpp
@@ -1,4 +1,27 @@
-/* Joy 2001 */
+/*
+ * mfp.cpp - MFP emulation
+ *
+ * Based on info gathered in STonX source code 
+ *
+ * Copyright (c) 2001-2003 Joy of ARAnyM development team (see AUTHORS)
+ *
+ * This file is part of the ARAnyM project which builds a new and powerful
+ * TOS/FreeMiNT compatible virtual machine running on almost any hardware.
+ *
+ * ARAnyM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ARAnyM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARAnyM; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include "sysdeps.h"
 #include "hardware.h"
@@ -9,249 +32,264 @@
 #define DEBUG 0
 #include "debug.h"
 
-static const int HW = 0xfffa00;
 
-	MFP_Timer::MFP_Timer(int value)
-	{
-		name = 'A' + value;
-		control = start_data = current_data = 0;
+MFP_Timer::MFP_Timer(int value)
+{
+	name = 'A' + value;
+	control = start_data = current_data = 0;
+	state = false;
+}
+
+bool MFP_Timer::isRunning()
+{
+	return ((control & 0x0f) > 0);
+}
+
+void MFP_Timer::setControl(uint8 value)
+{
+	control = value & 0x0f;
+	if (value & 0x10)
 		state = false;
+	// D(bug("Set MFP Timer%c control to $%x", name, value));
+}
+
+uint8 MFP_Timer::getControl()
+{
+	return control | (state << 5);
+}
+
+void MFP_Timer::setData(uint8 value)
+{
+	// D(bug("Set MFP Timer%c data to %d", name, value));
+	start_data = value;
+	if (! isRunning())
+		current_data = value;
+}
+
+void MFP_Timer::reset()
+{
+	// D(bug("reset of Timer%c", name));
+	if (isRunning()) {
+		state = true;
+		current_data = start_data;
 	}
+}
 
-	bool MFP_Timer::isRunning()
-	{
-		return ((control & 0x0f) > 0);
-	}
+uint8 MFP_Timer::getData()
+{
+	// D(bug("get MFP Timer%c data = %d", name, current_data));
 
-	void MFP_Timer::setControl(uint8 value)
-	{
-		control = value & 0x0f;
-		if (value & 0x10)
-			state = false;
-		// D(bug("Set MFP Timer%c control to $%x", name, value));
-	}
+	if (isRunning() && current_data > 2)
+		current_data--;		// hack to overcome microseconds delays in TOS (e.g. at $E02570)
 
-	uint8 MFP_Timer::getControl()
-	{
-		return control | (state << 5);
-	}
+	return current_data;
+}
 
-	void MFP_Timer::setData(uint8 value)
-	{
-		// D(bug("Set MFP Timer%c data to %d", name, value));
-		start_data = value;
-		if (! isRunning())
-			current_data = value;
-	}
+/*************************************************************************/
 
-	void MFP_Timer::reset()
-	{
-		// D(bug("reset of Timer%c", name));
-		if (isRunning()) {
-			state = true;
-			current_data = start_data;
-		}
-	}
+MFP::MFP(memptr addr, uint32 size) : BASE_IO(addr, size)
+{
+	GPIP_data = 0xff;
+	vr = 0x0100;
+	active_edge = 0;
+	data_direction = 0;
+	irq_enable = 0;
+	irq_pending = 0;
+	irq_inservice = 0;
+	irq_mask = 0;
+	automaticServiceEnd = 0;
+	// MFP_TimerA A;
+	// MFP_TimerB B;
+	// MFP_TimerC C;
+	// MFP_TimerD D;
+	// USART usart;
+	flags = 0;
+	timerCounter = 0;
+}
 
-	uint8 MFP_Timer::getData()
-	{
-		// D(bug("get MFP Timer%c data = %d", name, current_data));
+uint8 MFP::handleRead(uaecptr addr)
+{
+	addr -= getHWoffset();
+	if (addr > 0x2f)
+		return 0;	// unhandled
 
-		if (isRunning() && current_data > 2)
-			current_data--;		// hack to overcome microseconds delays in TOS (e.g. at $E02570)
+	uint8 value;
+	switch(addr) {
+		case 0x01:	value = (GPIP_data & ~ 0x21) | parallel.getBusy();
+					break;
 
-		return current_data;
-	}
+		case 0x03:	value = active_edge;
+					break;
 
-	/*************************************************************************/
+		case 0x05:	value = data_direction;
+					break;
 
-	MFP::MFP(memptr addr, uint32 size) : BASE_IO(addr, size)
-	{
-		GPIP_data = 0xff;
-		vr = 0x0100;
-	}
+		case 0x07:	value = irq_enable >> 8;
+					break;
 
-	uint8 MFP::handleRead(uaecptr addr)
-	{
-		addr -= HW;
-		if (addr > 0x2f)
-			return 0;	// unhandled
+		case 0x09:	value = irq_enable;
+					break;
 
-		uint8 value;
-		switch(addr) {
-			case 0x01:	value = (GPIP_data & ~ 0x21) | parallel.getBusy();
-						break;
+		case 0x0b:	value = 0x20; //(irq_pending >> 8) | (tA->getControl() & 0x10);	// finish
+					break;
 
-			case 0x03:	value = active_edge;
-						break;
+		case 0x0d:	// D(bug("Read: TimerC IRQ %s pending", (irq_pending & 0x20) ? "" : "NOT"));
+					value = irq_pending;
+					break;
 
-			case 0x05:	value = data_direction;
-						break;
+		case 0xf:	value = irq_inservice >> 8;
+					break;
 
-			case 0x07:	value = irq_enable >> 8;
-						break;
-
-			case 0x09:	value = irq_enable;
-						break;
-
-			case 0x0b:	value = 0x20; //(irq_pending >> 8) | (tA->getControl() & 0x10);	// finish
-						break;
-
-			case 0x0d:	// D(bug("Read: TimerC IRQ %s pending", (irq_pending & 0x20) ? "" : "NOT"));
-						value = irq_pending;
-						break;
-
-			case 0xf:	value = irq_inservice >> 8;
-						break;
-
-			case 0x11:	// D(bug("Read: TimerC IRQ %s in-service", (irq_inservice & 0x20) ? "" : "NOT"));
-						value = irq_inservice;
-						break;
+		case 0x11:	// D(bug("Read: TimerC IRQ %s in-service", (irq_inservice & 0x20) ? "" : "NOT"));
+					value = irq_inservice;
+					break;
 						
-			case 0x13:	value = irq_mask >> 8;
-						break;
+		case 0x13:	value = irq_mask >> 8;
+					break;
 						
-			case 0x15:	// D(bug("Read: TimerC IRQ %s masked", (irq_mask & 0x20) ? "" : "NOT"));
-						value = irq_mask;
-						break;
+		case 0x15:	// D(bug("Read: TimerC IRQ %s masked", (irq_mask & 0x20) ? "" : "NOT"));
+					value = irq_mask;
+					break;
 						
-			case 0x17:	value = (vr & 0xf0 ) | (automaticServiceEnd ? 8 : 0);
-						break;
+		case 0x17:	value = (vr & 0xf0 ) | (automaticServiceEnd ? 8 : 0);
+					break;
 
-			case 0x19:	value = A.getControl();
-						break;
+		case 0x19:	value = A.getControl();
+					break;
 
-			case 0x1b:	value = B.getControl();
-						break;
+		case 0x1b:	value = B.getControl();
+					break;
 
-			case 0x1d:	value = (C.getControl() << 4) | D.getControl();
-						break;
+		case 0x1d:	value = (C.getControl() << 4) | D.getControl();
+					break;
 
-			case 0x1f:	value = A.getData();
-						break;
+		case 0x1f:	value = A.getData();
+					break;
 
-			case 0x21:	value = B.getData();
-						break;
+		case 0x21:	value = B.getData();
+					break;
 
-			case 0x23:	value = C.getData();
-						break;
+		case 0x23:	value = C.getData();
+					break;
 						
-			case 0x25:	value = D.getData();
-						break;
+		case 0x25:	value = D.getData();
+					break;
 						
-			case 0x2d:	value = 0x80;	// for Linux/m68k
-						break;
+		case 0x2d:	value = 0x80;	// for Linux/m68k
+					break;
 
-			default: value = 0;
-		};
-		D(bug("Reading MFP data from %04lx = %d ($%02x) at %06x\n", addr, value, value, showPC()));
-		return value;
-	}
+		default: value = 0;
+	};
+	D(bug("Reading MFP data from %04lx = %d ($%02x) at %06x\n", addr, value, value, showPC()));
+	return value;
+}
 
-	void MFP::handleWrite(uaecptr addr, uint8 value) {
-		addr -= HW;
-		if (addr > 0x2f)
-			return;	// unhandled
+void MFP::handleWrite(uaecptr addr, uint8 value) {
+	addr -= getHWoffset();
+	if (addr > 0x2f)
+		return;	// unhandled
 
-		D(bug("Writing MFP data to %04lx = %d ($%02x) at %06x\n", addr, value, value, showPC()));
-		switch(addr) {
-			case 0x01:	//GPIP_data = value;
-						GPIP_data &= ~data_direction ;
-						GPIP_data |= value & data_direction;
-						D(bug("New GPIP=$%x from PC=$%x", GPIP_data, showPC()));
-						break;
+	D(bug("Writing MFP data to %04lx = %d ($%02x) at %06x\n", addr, value, value, showPC()));
+	switch(addr) {
+		case 0x01:	//GPIP_data = value;
+					GPIP_data &= ~data_direction ;
+					GPIP_data |= value & data_direction;
+					D(bug("New GPIP=$%x from PC=$%x", GPIP_data, showPC()));
+					break;
 
-			case 0x03:	active_edge = value;
-						break;
+		case 0x03:	active_edge = value;
+					break;
 
-			case 0x05:	data_direction = value;
-						break;
+		case 0x05:	data_direction = value;
+					break;
 
-			case 0x07:	irq_enable = (irq_enable & 0x00ff) | (value << 8);
-						break;
+		case 0x07:	irq_enable = (irq_enable & 0x00ff) | (value << 8);
+					break;
 
-			case 0x09:
-	#if DEBUG_IER
-						if ((irq_enable ^ value) & 0x20)
-							D(bug("Write: TimerC IRQ %sabled", (value & 20) ? "en" : "dis"));
-						if ((irq_enable ^ value) & 0x40)
-							D(bug("Write: IKBD IRQ %sabled", (value & 40) ? "en" : "dis"));
-	#endif /* DEBUG */
-						irq_enable = (irq_enable & 0xff00) | value;
-						break;
+		case 0x09:
+#if DEBUG_IER
+					if ((irq_enable ^ value) & 0x20) {
+						D(bug("Write: TimerC IRQ %sabled", (value & 20) ? "en" : "dis"));
+					}
+					if ((irq_enable ^ value) & 0x40) {
+						D(bug("Write: IKBD IRQ %sabled", (value & 40) ? "en" : "dis"));
+					}
+#endif /* DEBUG */
+					irq_enable = (irq_enable & 0xff00) | value;
+					break;
 
-			case 0x0b:	irq_pending = (irq_pending & 0x00ff) | (value << 8);
-						break;
+		case 0x0b:	irq_pending = (irq_pending & 0x00ff) | (value << 8);
+					break;
 
-			case 0x0d:
-	#if DEBUG_IPR
-						if ((irq_pending ^ value) & 0x20) {
-							D(bug("Write: TimerC IRQ %s pending", (value & 20) ? "" : "NOT"));
-						}
-	#endif /* DEBUG */
-						irq_pending = (irq_pending & 0xff00) | value;
-						break;
+		case 0x0d:
+#if DEBUG_IPR
+					if ((irq_pending ^ value) & 0x20) {
+						D(bug("Write: TimerC IRQ %s pending", (value & 20) ? "" : "NOT"));
+					}
+#endif /* DEBUG */
+					irq_pending = (irq_pending & 0xff00) | value;
+					break;
 
-			case 0xf:	irq_inservice = (irq_inservice & 0x00ff) | (value << 8);
-						break;
+		case 0xf:	irq_inservice = (irq_inservice & 0x00ff) | (value << 8);
+					break;
 
-			case 0x11:
-	#if DEBUG_ISR
-						if ((irq_inservice ^ value) & 0x20) {
-							D(bug("Write: TimerC IRQ %s in-service at %08x", (value & 20) ? "" : "NOT", showPC()));
-						}
-	#endif /* DEBUG */
-						irq_inservice = (irq_inservice & 0xff00) | (irq_inservice & value);
-						break;
-						
-			case 0x13:	irq_mask = (irq_mask & 0x00ff) | (value << 8);
-						break;
-						
-			case 0x15:
-	#if DEBUG_IMR
-						if ((irq_mask ^ value) & 0x20) {
-							D(bug("Write: TimerC IRQ %s masked", (value & 20) ? "" : "NOT"));
-						}
-	#endif /* DEBUG */
-						irq_mask = (irq_mask & 0xff00) | value;
-						break;
-						
-			case 0x17:	vr = value;
-						automaticServiceEnd = (value & 0x08) ? true : false;
-						// D(bug("MFP autoServiceEnd: %s", automaticServiceEnd ? "YES" : "NO"));
-						break;
+		case 0x11:
+#if DEBUG_ISR
+					if ((irq_inservice ^ value) & 0x20) {
+						D(bug("Write: TimerC IRQ %s in-service at %08x", (value & 20) ? "" : "NOT", showPC()));
+					}
+#endif /* DEBUG */
+					irq_inservice = (irq_inservice & 0xff00) | (irq_inservice & value);
+					break;
+					
+		case 0x13:	irq_mask = (irq_mask & 0x00ff) | (value << 8);
+					break;
+					
+		case 0x15:
+#if DEBUG_IMR
+					if ((irq_mask ^ value) & 0x20) {
+						D(bug("Write: TimerC IRQ %s masked", (value & 20) ? "" : "NOT"));
+					}
+#endif /* DEBUG */
+					irq_mask = (irq_mask & 0xff00) | value;
+					break;
+					
+		case 0x17:	vr = value;
+					automaticServiceEnd = (value & 0x08) ? true : false;
+					// D(bug("MFP autoServiceEnd: %s", automaticServiceEnd ? "YES" : "NO"));
+					break;
 
-			case 0x19:	A.setControl(value);
-						break;
+		case 0x19:	A.setControl(value);
+					break;
 
-			case 0x1b:	B.setControl(value);
-						break;
+		case 0x1b:	B.setControl(value);
+					break;
 
-			case 0x1d:	C.setControl(value >> 4);
-						D.setControl(value & 0x0f);
-						break;
+		case 0x1d:	C.setControl(value >> 4);
+					D.setControl(value & 0x0f);
+					break;
 
-			case 0x1f:	A.setData(value);
-						break;
+		case 0x1f:	A.setData(value);
+					break;
 
-			case 0x21:	B.setData(value);
-						break;
+		case 0x21:	B.setData(value);
+					break;
 
-			case 0x23:	C.setData(value);
-						break;
-						
-			case 0x25:	D.setData(value);
-						break;
-						
-			case 0x27:
-			case 0x29:
-			case 0x2b:
-			case 0x2d:
-			case 0x2f:
-			default:
-				break;
-		};
-	}
+		case 0x23:	C.setData(value);
+					break;
+					
+		case 0x25:	D.setData(value);
+					break;
+					
+		case 0x27:
+		case 0x29:
+		case 0x2b:
+		case 0x2d:
+		case 0x2f:
+		default:
+			break;
+	};
+}
 
 /*
  * setGPIPbit sets input port bits (peripheral interrupt request)
@@ -298,8 +336,7 @@ void MFP::IRQ(int no, int count)
 
 		// TimerC 200 Hz interrupt
 		case 5: C.reset();
-				if (irq_enable & 0x0020)
-				{
+				if (irq_enable & 0x0020) {
 					flags |= F_TIMERC;
 					timerCounter += count;
 					TriggerMFP(true);
