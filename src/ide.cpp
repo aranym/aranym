b9040e538935558f43d15f8e7763ee3384f74261
joy <>
2004-01-02 00:30:46 +0000

    init() added for runtime reinitialization

--- src/ide.cpp
+++ src/ide.cpp
@@ -1,15 +1,53 @@
-/* Joy 2001 */
+/*
+ * ide.cpp - Falcon IDE emulation code - wrapper for bochs' ata.cpp
+ *
+ * Copyright (c) 2001-2004 Petr Stehlik of ARAnyM dev team (see AUTHORS)
+ * 
+ * This file is part of the ARAnyM project which builds a new and powerful
+ * TOS/FreeMiNT compatible virtual machine running on almost any hardware.
+ *
+ * ARAnyM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ARAnyM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARAnyM; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 
 #include "sysdeps.h"
 #include "hardware.h"
-#include "cpu_emulation.h"
-#include "memory.h"
-#include "mmu.h"
 #include "parameters.h"
 #include "ide.h"
 #include "ata.h"
 
+#define BX_INSERTED	true	// copied from emu_bochs.h
+
 IDE::IDE(memptr addr, uint32 size) : BASE_IO(addr, size) {
+	init();
+}
+
+void IDE::init() {
+	// convert aranym's "present" and "isCDROM" to bochs' "status" and "type"
+	int channel = 0;
+	for(int device=0; device<2; device++) {
+    	if (bx_options.atadevice[channel][device].present) {
+    		bx_options.atadevice[channel][device].status = BX_INSERTED;
+			bx_options.atadevice[channel][device].type = (bx_options.atadevice[channel][device].isCDROM) ? IDE_CDROM : IDE_DISK;
+		}
+		else {
+			bx_options.atadevice[channel][device].type = IDE_NONE;
+		}
+	}
+
+	// init bochs emulation
 	bx_hard_drive.init();
 }
 
--- src/include/ide.h
+++ src/include/ide.h
@@ -1,10 +1,36 @@
-/* Joy 2001 */
+/*
+ * ide.h - Falcon IDE emulation code - class definition
+ *
+ * Copyright (c) 2001-2004 Petr Stehlik of ARAnyM dev team (see AUTHORS)
+ * 
+ * This file is part of the ARAnyM project which builds a new and powerful
+ * TOS/FreeMiNT compatible virtual machine running on almost any hardware.
+ *
+ * ARAnyM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ARAnyM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARAnyM; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#ifndef _IDE_H
+#define _IDE_H
 
 #include "icio.h"
 
 class IDE : public BASE_IO {
 public:
 	IDE(memptr, uint32);
+	void init();
 	virtual uae_u8 handleRead(uaecptr addr);
 	virtual void handleWrite(uaecptr addr, uae_u8 value);
 	virtual uae_u16 handleReadW(uaecptr addr);
@@ -12,3 +38,5 @@ public:
 	virtual uae_u32 handleReadL(uaecptr addr);
 	virtual void handleWriteL(uaecptr addr, uae_u32 value);
 };
+
+#endif /* _IDE_H */
