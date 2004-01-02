8527aaad04ea578c4372f0b0b190ca077108eeb1
joy <>
2004-01-02 00:30:12 +0000

    getIDE() added

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -1,3 +1,27 @@
+/*
+ * hardware.cpp - Atari hardware emulation
+ *
+ * Copyright (c) 2001-2004 Petr Stehlik of ARAnyM dev team (see AUTHORS)
+ * 
+ * This file is part of the ARAnyM project which builds a new and powerful
+ * TOS/FreeMiNT compatible virtual machine running on almost any hardware.
+ *
+ * ARAnyM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ARAnyM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARAnyM; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
 #include "sysdeps.h"
 #include "hardware.h"
 #include "cpu_emulation.h"
@@ -99,6 +123,7 @@ YAMAHA* getYAMAHA()	{ return yamaha; }
 VIDEL* getVIDEL()	{ return videl; }
 DSP* getDSP()		{ return dsp; }
 ACSIFDC *getFDC()	{ return fdc; }
+IDE *getIDE()		{ return ide; }
 
 uae_u32 HWget_l (uaecptr addr) {
 	D(bug("HWget_l %x <- %s at %08x", addr, debug_print_IO(addr), showPC()));
--- src/include/hardware.h
+++ src/include/hardware.h
@@ -1,7 +1,26 @@
 /*
- * $Header$
+ * hardware.h - Atari hardware emulation - definitions
  *
+ * Copyright (c) 2001-2004 Petr Stehlik of ARAnyM dev team (see AUTHORS)
+ * 
+ * This file is part of the ARAnyM project which builds a new and powerful
+ * TOS/FreeMiNT compatible virtual machine running on almost any hardware.
+ *
+ * ARAnyM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ARAnyM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARAnyM; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 
 #ifndef HARDWARE_H
 #define HARDWARE_H
@@ -15,6 +34,7 @@
 #include "videl.h"
 #include "dsp.h"
 #include "acsifdc.h"
+#include "ide.h"
 
 extern Parallel parallel;
 
@@ -31,6 +51,7 @@ YAMAHA *getYAMAHA(void);
 VIDEL *getVIDEL(void);
 DSP *getDSP(void);
 ACSIFDC *getFDC(void);
+IDE *getIDE(void);
 
 extern uae_u32 HWget_l(uaecptr addr);
 extern uae_u16 HWget_w(uaecptr addr);
