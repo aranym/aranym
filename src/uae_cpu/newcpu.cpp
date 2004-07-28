4c310f2dbf6c6ab53dca11e1dc33964e36e6b520
milan <>
2004-07-28 18:33:44 +0000

    removing "patch" from 2002/09/28 (PureC hack)

--- src/uae_cpu/newcpu.cpp
+++ src/uae_cpu/newcpu.cpp
@@ -747,7 +747,11 @@ void Exception(int nr, uaecptr oldpc)
 	exc_push_word(0);	/* WB2S */
 	exc_push_word(0);	/* WB3S */
 	exc_push_word(regs.mmu_ssw);
+#ifdef PUREC
 	exc_push_long(0 /* was regs.mmu_fault_addr */);	/* EA *//* bullshit here, took 10 hours to debug with PureC $12345678 test. It should be an internal register, keep 0 to preserve MSP in PureC */
+#else
+	exc_push_long(regs.mmu_fault_addr); /* EA *//* ARAnyM has 040, so stack format 7, not 000 stack format */
+#endif
 	exc_make_frame(7, regs.sr, currpc, 2, 0, 0);
 
 	building_bus_fault_stack_frame=0;
