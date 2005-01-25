1241bac2bebe37bca1060f8e3d2b2e04515c8fce
standa <>
2005-01-25 14:12:45 +0000

    Fixing the Makefiles to be compiled for 020-60 just as the kernel.
    Also make.sh can now handle paths with spaces.

--- atari/hostfs/make.sh
+++ atari/hostfs/make.sh
@@ -1,26 +1,26 @@
-if [ x$FREEMINT_SRC_HOME == x ]; then 
+if [ x"$FREEMINT_SRC_HOME" = x ]; then 
 	FREEMINT_SRC_HOME=/local/home/standa/projects/mint/freemint
 fi
 
-if [ ! -e $FREEMINT_SRC_HOME/COPYING.MiNT ]; then 
+if [ ! -e "$FREEMINT_SRC_HOME/COPYING.MiNT" ]; then 
 	echo "The $FREEMINT_SRC_HOME seems not to be the FreeMiNT CVS root";
 	exit 1;
 fi
 
-HOSTFS_BUILD_HOME=$FREEMINT_SRC_HOME/sys/xfs
+HOSTFS_BUILD_HOME="$FREEMINT_SRC_HOME/sys/xfs"
 
 # copy the sources into the FreeMiNT cvs tree
-cp -r ../hostfs $HOSTFS_BUILD_HOME
-cp -r ../natfeat $HOSTFS_BUILD_HOME
+cp -r ../hostfs "$HOSTFS_BUILD_HOME"
+cp -r ../natfeat "$HOSTFS_BUILD_HOME"
 
 # build them (all or debug targets are available)
-(cd $HOSTFS_BUILD_HOME/hostfs; make distclean; make $@)
+(cd "$HOSTFS_BUILD_HOME/hostfs"; make distclean; make $@)
 
 # move the binaries into the bin folder
 mkdir -p bin
-cp $HOSTFS_BUILD_HOME/hostfs/metados/*.dos bin
-cp $HOSTFS_BUILD_HOME/hostfs/*.xfs bin
+cp "$HOSTFS_BUILD_HOME"/hostfs/metados/*.dos bin
+cp "$HOSTFS_BUILD_HOME"/hostfs/*.xfs bin
 
 # cleanup the FreeMiNT cvs tree
-#rm -rf $HOSTFS_BUILD_HOME/hostfs
-#rm -rf $HOSTFS_BUILD_HOME/natfeat
+#rm -rf "$HOSTFS_BUILD_HOME/hostfs"
+#rm -rf "$HOSTFS_BUILD_HOME/natfeat"
