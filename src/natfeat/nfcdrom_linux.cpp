50579a175673c1f4f065da3b81f9ff3cccaba4a4
schwab <>
2005-07-16 18:10:46 +0000

    Fix compilation problem.

--- src/natfeat/nfcdrom_linux.cpp
+++ src/natfeat/nfcdrom_linux.cpp
@@ -18,10 +18,10 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include <linux/cdrom.h>
-
 #include <SDL_cdrom.h>
 #include <SDL_endian.h>
+
+#include <linux/cdrom.h>
 
 #include "sysdeps.h"
 #include "cpu_emulation.h"
