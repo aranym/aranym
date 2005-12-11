4c6b5ec3c4f53d4820aabe6da94f1cde23098d23
pmandin <>
2005-12-11 14:18:54 +0000

    Validate BootOs changes

--- src/main.cpp
+++ src/main.cpp
@@ -37,13 +37,7 @@
 #include "version.h"		// for heartBeat
 #include "natfeat/nf_objs.h"
 #include "audio.h"
-#ifdef ENABLE_CXX_EXCEPTIONS
-#include "rom.h"
-#else
-# ifdef ENABLE_LILO
-# include "lilo.h"
-# endif
-#endif
+#include "bootos.h"
 
 #define DEBUG 0
 #include "debug.h"
@@ -341,253 +335,6 @@ Uint32 my_callback_function(Uint32 /*interval*/, void * /*param*/)
 }
 #endif
 
-#ifndef ENABLE_CXX_EXCEPTIONS
-/*
- * Load, check and patch the TOS 4.04 ROM image file
- */
-bool InitTOSROM(void)
-{
-	if (strlen(bx_options.tos_path) == 0)
-		return false;
-
-	// read ROM file
-	D(bug("Reading TOS image '%s'", bx_options.tos_path));
-	FILE *f = fopen(bx_options.tos_path, "rb");
-	if (f == NULL) {
-		panicbug("TOS image '%s' not found.", bx_options.tos_path);
-		return false;
-	}
-
-	RealROMSize = 512 * 1024;
-	if (fread(ROMBaseHost, 1, RealROMSize, f) != (size_t)RealROMSize) {
-		panicbug("TOS image '%s' reading error.\nMake sure the file is readable and its size is 524288 bytes (512 kB).", bx_options.tos_path);
-		fclose(f);
-		return false;
-	}
-	fclose(f);
-
-	// check if this is the correct 68040 aware TOS ROM version
-	D(bug("Checking TOS version.."));
-	unsigned char TOS404[16] = {0xe5,0xea,0x0f,0x21,0x6f,0xb4,0x46,0xf1,0xc4,0xa4,0xf4,0x76,0xbc,0x5f,0x03,0xd4};
-	MD5 md5;
-	unsigned char loadedTOS[16];
-	md5.computeSum(ROMBaseHost, RealROMSize, loadedTOS);
-	if (memcmp(loadedTOS, TOS404, 16) == 0) {
-		// patch it for 68040 compatibility
-		D(bug("Patching TOS 4.04 for 68040 compatibility.."));
-		int ptr, i=0;
-		while((ptr=tosdiff[i].offset) >= 0)
-			ROMBaseHost[ptr] = tosdiff[i++].newvalue;
-	}
-	else {
-		panicbug("Wrong TOS version. You need the original TOS 4.04.");
-		return false;
-	}
-
-	// patch cookies
-	// _MCH
-	ROMBaseHost[0x00416] = bx_options.tos.cookie_mch >> 24;
-	ROMBaseHost[0x00417] = (bx_options.tos.cookie_mch >> 16) & 0xff;
-	ROMBaseHost[0x00418] = (bx_options.tos.cookie_mch >> 8) & 0xff;
-	ROMBaseHost[0x00419] = (bx_options.tos.cookie_mch) & 0xff;
-	// _SND
-#if DSP_EMULATION
-	ROMBaseHost[0x00437] = 0x0E;	/* DSP, XBIOS, DMA8 */
-#else
-	ROMBaseHost[0x00437] = 0x06;	/* XBIOS, DMA8 */
-#endif
-
-	/*
-		Remove TOS check for joypads port that set DMA audio bit to 0 if not
-		present
-	*/
-	ROMBaseHost[0x0043e] = 0x4e;	/* btst #7,d0 (d0 is value on joypad port) */
-	ROMBaseHost[0x0043f] = 0x71;
-	ROMBaseHost[0x00440] = 0x4e;
-	ROMBaseHost[0x00441] = 0x71;
-	ROMBaseHost[0x00442] = 0x4e;	/* bne.s 0xe00448 */
-	ROMBaseHost[0x00443] = 0x71;
-	ROMBaseHost[0x00444] = 0x4e;	/* bclr #1,d1 (d1 is value of _SND cookie) */
-	ROMBaseHost[0x00445] = 0x71;
-	ROMBaseHost[0x00446] = 0x4e;
-	ROMBaseHost[0x00447] = 0x71;
-
-	if (FastRAMSize > 0) {
-		int i;
-
-		// patch to show FastRAM memory test
-		i = 0x001CC;
-		ROMBaseHost[i++] = 0x4E;
-		ROMBaseHost[i++] = 0xF9;	// JMP <abs.addr>
-		ROMBaseHost[i++] = 0x00;	// Can't use JSR, stack pointer
-		ROMBaseHost[i++] = 0xE7;	// is not initialized yet.
-		ROMBaseHost[i++] = 0xFF;
-		ROMBaseHost[i++] = 0x00;	// abs.addr = $E7FF00
-
-		i = 0x7FF00;
-		uint32 ramtop = (FastRAMBase + FastRAMSize);
-		ROMBaseHost[i++] = 0x21;
-		ROMBaseHost[i++] = 0xFC;	// MOVE.L #imm, abs.addr.w
-		ROMBaseHost[i++] = ramtop >> 24;
-		ROMBaseHost[i++] = ramtop >> 16;
-		ROMBaseHost[i++] = ramtop >> 8;
-		ROMBaseHost[i++] = ramtop;
-		ROMBaseHost[i++] = 0x05;
-		ROMBaseHost[i++] = 0xA4;	// abs.addr.w = $5A4 - ramtop
-		ROMBaseHost[i++] = 0x4E;
-		ROMBaseHost[i++] = 0xF9;	// JMP <abs.addr>
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0xE0;
-		ROMBaseHost[i++] = 0x01;
-		ROMBaseHost[i++] = 0xD2;	// abs.addr = $E001D2
-
-		// Patch to make FastRAM available to GEMDOS
-		i = 0x0096E;
-		ROMBaseHost[i++] = 0x4E;
-		ROMBaseHost[i++] = 0xB9;	// JSR <abs.addr>
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0xE7;
-		ROMBaseHost[i++] = 0xFF;
-		ROMBaseHost[i++] = 0x0E;	// abs.addr = $E7FF0E
-
-		i = 0x7FF0E;
-		// Declare FastRAM with Maddalt()
-		ROMBaseHost[i++] = 0x2F;
-		ROMBaseHost[i++] = 0x3C;	// MOVE.L #imm, -(sp)
-		ROMBaseHost[i++] = FastRAMSize >> 24;
-		ROMBaseHost[i++] = FastRAMSize >> 16;
-		ROMBaseHost[i++] = FastRAMSize >> 8;
-		ROMBaseHost[i++] = FastRAMSize;
-		ROMBaseHost[i++] = 0x2F;
-		ROMBaseHost[i++] = 0x3C;	// MOVE.L #imm, -(sp)
-		ROMBaseHost[i++] = FastRAMBase >> 24;
-		ROMBaseHost[i++] = FastRAMBase >> 16;
-		ROMBaseHost[i++] = FastRAMBase >> 8;
-		ROMBaseHost[i++] = FastRAMBase;
-		ROMBaseHost[i++] = 0x3F;
-		ROMBaseHost[i++] = 0x3C;	// MOVE.W #imm, -(sp)
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0x14;	// imm = $14 - Maddalt()
-		ROMBaseHost[i++] = 0x4E;
-		ROMBaseHost[i++] = 0x41;	// TRAP	#1
-		ROMBaseHost[i++] = 0x4F;
-		ROMBaseHost[i++] = 0xEF;	// LEA d16(sp), sp
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0x0A;	// d16 = $A
-
-		// Allocate 64k _FRB buffer
-		ROMBaseHost[i++] = 0x42;
-		ROMBaseHost[i++] = 0x67;	// CLR.W -(sp) - ST-Ram only
-		ROMBaseHost[i++] = 0x2F;
-		ROMBaseHost[i++] = 0x3C;	// MOVE.L #imm, -(sp)
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0x01;
-		ROMBaseHost[i++] = 0x20;
-		ROMBaseHost[i++] = 0x00;	// imm = 64k + 8k
-		ROMBaseHost[i++] = 0x3F;
-		ROMBaseHost[i++] = 0x3C;	// MOVE.W #imm, -(sp)
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0x44;	// imm = $44 - Mxalloc()
-		ROMBaseHost[i++] = 0x4E;
-		ROMBaseHost[i++] = 0x41;	// TRAP	#1
-		ROMBaseHost[i++] = 0x50;
-		ROMBaseHost[i++] = 0x4F;	// ADDQ.W #8,sp
-		ROMBaseHost[i++] = 0x06;
-		ROMBaseHost[i++] = 0x80;	// ADDI.L #imm, d0
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0x1F;
-		ROMBaseHost[i++] = 0xFF;	// imm = 8k - 1
-		ROMBaseHost[i++] = 0x02;
-		ROMBaseHost[i++] = 0x40;	// ANDI.W #imm, d0
-		ROMBaseHost[i++] = 0xE0;
-		ROMBaseHost[i++] = 0x00;	// imm = $E000 - 8k alignment
-
-		// Store _FRB cookie
-		ROMBaseHost[i++] = 0x20;
-		ROMBaseHost[i++] = 0x78;	// MOVEA.L addr.w, a0
-		ROMBaseHost[i++] = 0x05;
-		ROMBaseHost[i++] = 0xA0;	// addr.w = 0x5A0 - cookie jar
-		ROMBaseHost[i++] = 0x51;
-		ROMBaseHost[i++] = 0x48;	// SUBQ.W #8,a0
-		// .find_last_cookie:
-		ROMBaseHost[i++] = 0x50;
-		ROMBaseHost[i++] = 0x48;	// ADDQ.W #8,a0
-		ROMBaseHost[i++] = 0x4A;
-		ROMBaseHost[i++] = 0x90;	// TST.L (a0)
-		ROMBaseHost[i++] = 0x66;
-		ROMBaseHost[i++] = 0xFA;	// BNE.S .find_last_cookie
-		ROMBaseHost[i++] = 0x20;
-		ROMBaseHost[i++] = 0xFC;	// MOVE.L #imm, (a0)+
-		ROMBaseHost[i++] = 0x5F;
-		ROMBaseHost[i++] = 0x46;
-		ROMBaseHost[i++] = 0x52;
-		ROMBaseHost[i++] = 0x42;	// imm = '_FRB'
-		ROMBaseHost[i++] = 0x21;
-		ROMBaseHost[i++] = 0x50;	// MOVE.L (a0), 8(a0)
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0x08;	// d16 = 8 - copy jar size
-		ROMBaseHost[i++] = 0x20;
-		ROMBaseHost[i++] = 0xC0;	// MOVE.L d0,(a0)+
-		ROMBaseHost[i++] = 0x42;
-		ROMBaseHost[i++] = 0x90;	// CLR.L (a0)
-
-		// Code overwritten by JSR
-		ROMBaseHost[i++] = 0x70;
-		ROMBaseHost[i++] = 0x03;	// MOVEQ.L #3,d0
-		ROMBaseHost[i++] = 0x4E;
-		ROMBaseHost[i++] = 0xB9;	// JSR <abs.addr>
-		ROMBaseHost[i++] = 0x00;
-		ROMBaseHost[i++] = 0xE0;
-		ROMBaseHost[i++] = 0x0B;
-		ROMBaseHost[i++] = 0xD2;	// abs.addr = $E00BD2
-		ROMBaseHost[i++] = 0x4E;
-		ROMBaseHost[i++] = 0x75;	// RTS
-	}
-
-	// Xconout patch
-	if (bx_options.tos.redirect_CON) {
-		ROMBaseHost[0x8d44] = ROMBaseHost[0x8d50] = 0x71;
-		ROMBaseHost[0x8d45] = ROMBaseHost[0x8d51] = 0x2a;
-		ROMBaseHost[0x8d46] = ROMBaseHost[0x8d52] = 0x4e;
-		ROMBaseHost[0x8d47] = ROMBaseHost[0x8d53] = 0x75;
-	}
-
-	infoprint("TOS 4.04 loading... [OK]");
-	return true;
-}
-
-/*
- * Load EmuTOS image file
- */
-bool InitEmuTOS(void)
-{
-	if (strlen(bx_options.emutos_path) == 0)
-		return false;
-
-	// read EmuTOS file
-	D(bug("Reading EmuTOS from '%s'", bx_options.emutos_path));
-	FILE *f = fopen(bx_options.emutos_path, "rb");
-	if (f == NULL) {
-		panicbug("EmuTOS image '%s' not found.", bx_options.emutos_path);
-		return false;
-	}
-	RealROMSize = 512 * 1024;
-	bool bEmuOK = (fread(ROMBaseHost, 1, RealROMSize, f) > 0);
-	fclose(f);
-	if (bEmuOK) {
-		infoprint("EmuTOS %02x%02x/%02x/%02x loading from '%s'... [OK]",
-			ROMBaseHost[0x18], ROMBaseHost[0x19],
-			ROMBaseHost[0x1a],
-			ROMBaseHost[0x1b],
-			bx_options.emutos_path);
-	}
-	else
-		panicbug("EmuTOS image '%s' reading error.", bx_options.tos_path);
-	return bEmuOK;
-}
-#endif /* ENABLE_CXX_EXCEPTIONS */
-
 /*
  * Initialize the Operating System - Linux, TOS 4.04 or EmuTOS
  */
@@ -601,50 +348,40 @@ bool InitOS(void)
 	 * a nice fallback.
 	 */
 
-#ifdef ENABLE_CXX_EXCEPTIONS
-	 
-	bool result = true;
-
+	/* Try Linux/m68k */
 	try {
 		if (boot_lilo) {
-			rom = new LinuxRom();
-		} else if (!boot_emutos) {
-			rom = new TosRom();
-		} else {
-			rom = new EmutosRom();
+			bootOs = new LinuxBootOs();
+			return true;
 		}
-	} catch (RomInitializationException &e) {
-		panicbug("No operating system found. ARAnyM can not boot!");
-		panicbug("Visit http://emutos.sourceforge.net/ and get your copy of EmuTOS now.");
-		result = false;
+	} catch (BootOsException &e) {
+		/* Could not init Linux/m68k */
+		panicbug(e.getErrorMessage());
 	}
 
-	return result;
+	/* Try TOS */
+	try {
+		if (!boot_emutos) {
+			bootOs = new TosBootOs();
+			return true;
+		}
+	} catch (BootOsException &e) {
+		/* Could not init TOS */
+		panicbug(e.getErrorMessage());
+	}
 
-#else
-
-# ifdef ENABLE_LILO
-	if (boot_lilo && LiloInit())
+	/* Try EmuTOS */
+	try {
+		bootOs = new EmutosBootOs();
 		return true;
-# endif
-
-	bool isOS = false;
-	if (!boot_emutos && InitTOSROM())
-		isOS = true;
-	else if (InitEmuTOS())
-		isOS = true;
-
-	if (isOS) {
-		// Setting "SP & PC" for TOS and EmuTOS
-		for (int i = 0; i < 8; i++)
-			RAMBaseHost[i] = ROMBaseHost[i];
-		return true;
+	} catch (BootOsException &e) {
+		/* Could not init EmuTOS */
+		panicbug(e.getErrorMessage());
 	}
 
 	panicbug("No operating system found. ARAnyM can not boot!");
 	panicbug("Visit http://emutos.sourceforge.net/ and get your copy of EmuTOS now.");
 	return false;
-#endif /* ENABLE_CXX_EXCEPTIONS */
 }
 
 
@@ -779,15 +516,7 @@ bool InitAll(void)
 
 void ExitAll(void)
 {
-#ifdef ENABLE_CXX_EXCEPTIONS
-	delete rom;
-#else
-# ifdef ENABLE_LILO
-	if (boot_lilo) {
-		LiloShutdown();
-	}
-# endif
-#endif
+	delete bootOs;
 
  	/* Close opened joystick */
  	if (SDL_NumJoysticks()>0) {
@@ -843,11 +572,7 @@ void RestartAll()
 	// HW reset provided by the RESET instruction hook
 
 	// OS init
-#ifdef ENABLE_CXX_EXCEPTIONS
-	rom->reset();
-#else
-	InitOS();
-#endif
+	bootOs->reset();
 
 	// CPU init
 	Restart680x0();
