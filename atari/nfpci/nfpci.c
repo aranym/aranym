f8bf74a87a8119016c0777b2c0f6c21c9e167db1
pmandin <>
2006-02-03 16:57:16 +0000

    nfpci updated for nf_ops.h

--- atari/nfpci/nfpci.c
+++ atari/nfpci/nfpci.c
@@ -26,7 +26,7 @@
 #include <mint/cookie.h>
 #include <mint/osbind.h>
 
-#include "../natfeat/natfeat.h"
+#include "../natfeat/nf_ops.h"
 #include "nfpci_nfapi.h"
 #include "nfpci.h"
 #include "nfpci_bios.h"
@@ -42,10 +42,6 @@
 #define C__PCI	0x5f504349L
 #endif
 
-#ifndef C___NF
-#define C___NF	0x5f5f4e46L
-#endif
-
 #ifndef VEC_XBIOS
 #define VEC_XBIOS	0x2e
 #endif
@@ -55,7 +51,7 @@
 #endif
 
 #define DRIVER_NAME	"ARAnyM host PCI driver"
-#define VERSION	"v0.2"
+#define VERSION	"v0.3"
 
 /*--- Types ---*/
 
@@ -126,6 +122,7 @@ extern void (*pcixbios_oldtrap)(void);
 
 /*--- Functions prototypes ---*/
 
+static struct nf_ops *nfOps;
 static void install_pci_bios(void);
 static void install_pci_xbios(void);
 static void press_any_key(void);
@@ -134,8 +131,6 @@ static void press_any_key(void);
 
 void install_driver(unsigned long resident_length)
 {
-	unsigned long cookie_nf;
-
 	Cconws(
 		"\033p " DRIVER_NAME " " VERSION " \033q\r\n"
 		"Copyright (c) ARAnyM Development Team, " __DATE__ "\r\n"
@@ -155,13 +150,14 @@ void install_driver(unsigned long resident_length)
 	}	
 
 	/* Check if NF is present for PCI */
-	if (!cookie_present(C___NF, &cookie_nf)) {
+	nfOps = nf_init();
+	if (!nfOps) {
 		Cconws("__NF cookie not present on this system\r\n");
 		press_any_key();
 		return;
 	}	
 
-	nfPciId = nfGetID(("PCI"));
+	nfPciId = nfOps->get_id("PCI");
 	if (nfPciId==0) {
 		Cconws("NF PCI functions not present on this system\r\n");
 		press_any_key();
@@ -202,72 +198,72 @@ static void install_pci_xbios(void)
 
 long find_device(unsigned long device_vendor_id, unsigned long index)
 {
-	return nfCall((NFPCI(NFPCI_FIND_DEVICE), device_vendor_id, index));
+	return nfOps->call(NFPCI(NFPCI_FIND_DEVICE), device_vendor_id, index);
 }
 
 long find_classcode(unsigned long class_code, unsigned long index)
 {
-	return nfCall((NFPCI(NFPCI_FIND_CLASSCODE), class_code, index));
+	return nfOps->call(NFPCI(NFPCI_FIND_CLASSCODE), class_code, index);
 }
 
 long read_config_byte(unsigned long device_handle, void *data, unsigned long num_register)
 {
-	return nfCall((NFPCI(NFPCI_READ_CONFIG_BYTE), device_handle, data, num_register));
+	return nfOps->call(NFPCI(NFPCI_READ_CONFIG_BYTE), device_handle, data, num_register);
 }
 
 long read_config_word(unsigned long device_handle, void *data, unsigned long num_register)
 {
-	return nfCall((NFPCI(NFPCI_READ_CONFIG_WORD), device_handle, data, num_register));
+	return nfOps->call(NFPCI(NFPCI_READ_CONFIG_WORD), device_handle, data, num_register);
 }
 
 long read_config_long(unsigned long device_handle, void *data, unsigned long num_register)
 {
-	return nfCall((NFPCI(NFPCI_READ_CONFIG_LONG), device_handle, data, num_register));
+	return nfOps->call(NFPCI(NFPCI_READ_CONFIG_LONG), device_handle, data, num_register);
 }
 
 unsigned char read_config_byte_fast(unsigned long device_handle, unsigned long num_register)
 {
-	return nfCall((NFPCI(NFPCI_READ_CONFIG_BYTE_FAST), device_handle, num_register));
+	return nfOps->call(NFPCI(NFPCI_READ_CONFIG_BYTE_FAST), device_handle, num_register);
 }
 
 unsigned short read_config_word_fast(unsigned long device_handle, unsigned long num_register)
 {
-	return nfCall((NFPCI(NFPCI_READ_CONFIG_WORD_FAST), device_handle, num_register));
+	return nfOps->call(NFPCI(NFPCI_READ_CONFIG_WORD_FAST), device_handle, num_register);
 }
 
 unsigned long read_config_long_fast(unsigned long device_handle, unsigned long num_register)
 {
-	return nfCall((NFPCI(NFPCI_READ_CONFIG_LONG_FAST), device_handle, num_register));
+	return nfOps->call(NFPCI(NFPCI_READ_CONFIG_LONG_FAST), device_handle, num_register);
 }
 
 long write_config_byte(unsigned long device_handle, unsigned long num_register, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_CONFIG_BYTE), device_handle, num_register, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_CONFIG_BYTE), device_handle, num_register, value);
 }
 
 long write_config_word(unsigned long device_handle, unsigned long num_register, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_CONFIG_WORD), device_handle, num_register, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_CONFIG_WORD), device_handle, num_register, value);
 }
 
 long write_config_long(unsigned long device_handle, unsigned long num_register, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_CONFIG_LONG), device_handle, num_register, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_CONFIG_LONG), device_handle, num_register, value);
 }
 
 long hook_interrupt(unsigned long device_handle, void (*data)(), unsigned long parameter)
 {
-	return nfCall((NFPCI(NFPCI_HOOK_INTERRUPT), device_handle, data, parameter));
+	return nfOps->call(NFPCI(NFPCI_HOOK_INTERRUPT), device_handle, data, parameter);
 }
  
 long unhook_interrupt(unsigned long device_handle)
 {
-	return nfCall((NFPCI(NFPCI_UNHOOK_INTERRUPT), device_handle));
+	return nfOps->call(NFPCI(NFPCI_UNHOOK_INTERRUPT), device_handle);
 }
  
 long special_cycle(unsigned long num_bus, unsigned long data)
 {
-	return nfCall((NFPCI(NFPCI_SPECIAL_CYCLE), num_bus, data));
+	return nfOps->call(NFPCI(NFPCI_SPECIAL_CYCLE), num_bus, data);
 }
  
 /* get_routing */
@@ -276,135 +272,135 @@ long special_cycle(unsigned long num_bus, unsigned long data)
 
 long get_resource(unsigned long device_handle)
 {
-	return nfCall((NFPCI(NFPCI_GET_RESOURCE), device_handle));
+	return nfOps->call(NFPCI(NFPCI_GET_RESOURCE), device_handle);
 }
  
 long get_card_used(unsigned long device_handle, unsigned long *callback)
 {
-	return nfCall((NFPCI(NFPCI_GET_CARD_USED), device_handle, callback));
+	return nfOps->call(NFPCI(NFPCI_GET_CARD_USED), device_handle, callback);
 }
 
 long set_card_used(unsigned long device_handle, unsigned long callback)
 {
-	return nfCall((NFPCI(NFPCI_SET_CARD_USED), device_handle, callback));
+	return nfOps->call(NFPCI(NFPCI_SET_CARD_USED), device_handle, callback);
 }
 
 long read_mem_byte(unsigned long device_handle, unsigned long pci_address, unsigned char *data)
 {
-	return nfCall((NFPCI(NFPCI_READ_MEM_BYTE), device_handle, pci_address, data));
+	return nfOps->call(NFPCI(NFPCI_READ_MEM_BYTE), device_handle, pci_address, data);
 }
 
 long read_mem_word(unsigned long device_handle, unsigned long pci_address, unsigned short *data)
 {
-	return nfCall((NFPCI(NFPCI_READ_MEM_WORD), device_handle, pci_address, data));
+	return nfOps->call(NFPCI(NFPCI_READ_MEM_WORD), device_handle, pci_address, data);
 }
 
 long read_mem_long(unsigned long device_handle, unsigned long pci_address, unsigned long *data)
 {
-	return nfCall((NFPCI(NFPCI_READ_MEM_LONG), device_handle, pci_address, data));
+	return nfOps->call(NFPCI(NFPCI_READ_MEM_LONG), device_handle, pci_address, data);
 }
 
 unsigned char read_mem_byte_fast(unsigned long device_handle, unsigned long pci_address)
 {
-	return nfCall((NFPCI(NFPCI_READ_MEM_BYTE_FAST), device_handle, pci_address));
+	return nfOps->call(NFPCI(NFPCI_READ_MEM_BYTE_FAST), device_handle, pci_address);
 }
 
 unsigned short read_mem_word_fast(unsigned long device_handle, unsigned long pci_address)
 {
-	return nfCall((NFPCI(NFPCI_READ_MEM_WORD_FAST), device_handle, pci_address));
+	return nfOps->call(NFPCI(NFPCI_READ_MEM_WORD_FAST), device_handle, pci_address);
 }
 
 unsigned long read_mem_long_fast(unsigned long device_handle, unsigned long pci_address)
 {
-	return nfCall((NFPCI(NFPCI_READ_MEM_LONG_FAST), device_handle, pci_address));
+	return nfOps->call(NFPCI(NFPCI_READ_MEM_LONG_FAST), device_handle, pci_address);
 }
 
 long write_mem_byte(unsigned long device_handle, unsigned long pci_address, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_MEM_BYTE), device_handle, pci_address, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_MEM_BYTE), device_handle, pci_address, value);
 }
 
 long write_mem_word(unsigned long device_handle, unsigned long pci_address, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_MEM_WORD), device_handle, pci_address, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_MEM_WORD), device_handle, pci_address, value);
 }
 
 long write_mem_long(unsigned long device_handle, unsigned long pci_address, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_MEM_LONG), device_handle, pci_address, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_MEM_LONG), device_handle, pci_address, value);
 }
 
 long read_io_byte(unsigned long device_handle, unsigned long pci_address, unsigned char *data)
 {
-	return nfCall((NFPCI(NFPCI_READ_IO_BYTE), device_handle, pci_address, data));
+	return nfOps->call(NFPCI(NFPCI_READ_IO_BYTE), device_handle, pci_address, data);
 }
 
 long read_io_word(unsigned long device_handle, unsigned long pci_address, unsigned short *data)
 {
-	return nfCall((NFPCI(NFPCI_READ_IO_WORD), device_handle, pci_address, data));
+	return nfOps->call(NFPCI(NFPCI_READ_IO_WORD), device_handle, pci_address, data);
 }
 
 long read_io_long(unsigned long device_handle, unsigned long pci_address, unsigned long *data)
 {
-	return nfCall((NFPCI(NFPCI_READ_IO_LONG), device_handle, pci_address, data));
+	return nfOps->call(NFPCI(NFPCI_READ_IO_LONG), device_handle, pci_address, data);
 }
 
 unsigned char read_io_byte_fast(unsigned long device_handle, unsigned long pci_address)
 {
-	return nfCall((NFPCI(NFPCI_READ_IO_BYTE_FAST), device_handle, pci_address));
+	return nfOps->call(NFPCI(NFPCI_READ_IO_BYTE_FAST), device_handle, pci_address);
 }
 
 unsigned short read_io_word_fast(unsigned long device_handle, unsigned long pci_address)
 {
-	return nfCall((NFPCI(NFPCI_READ_IO_WORD_FAST), device_handle, pci_address));
+	return nfOps->call(NFPCI(NFPCI_READ_IO_WORD_FAST), device_handle, pci_address);
 }
 
 unsigned long read_io_long_fast(unsigned long device_handle, unsigned long pci_address)
 {
-	return nfCall((NFPCI(NFPCI_READ_IO_LONG_FAST), device_handle, pci_address));
+	return nfOps->call(NFPCI(NFPCI_READ_IO_LONG_FAST), device_handle, pci_address);
 }
 
 long write_io_byte(unsigned long device_handle, unsigned long pci_address, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_IO_BYTE), device_handle, pci_address, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_IO_BYTE), device_handle, pci_address, value);
 }
 
 long write_io_word(unsigned long device_handle, unsigned long pci_address, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_IO_WORD), device_handle, pci_address, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_IO_WORD), device_handle, pci_address, value);
 }
 
 long write_io_long(unsigned long device_handle, unsigned long pci_address, unsigned long value)
 {
-	return nfCall((NFPCI(NFPCI_WRITE_IO_LONG), device_handle, pci_address, value));
+	return nfOps->call(NFPCI(NFPCI_WRITE_IO_LONG), device_handle, pci_address, value);
 }
 
 long get_machine_id(void)
 {
-	return nfCall((NFPCI(NFPCI_GET_MACHINE_ID)));
+	return nfOps->call(NFPCI(NFPCI_GET_MACHINE_ID));
 }
 
 unsigned long get_pagesize(void)
 {
-	return nfCall((NFPCI(NFPCI_GET_PAGESIZE)));
+	return nfOps->call(NFPCI(NFPCI_GET_PAGESIZE));
 }
 
 long virt_to_bus(unsigned long device_handle, void *virt_cpu_address, void *data)
 {
-	return nfCall((NFPCI(NFPCI_VIRT_TO_BUS), device_handle, virt_cpu_address, data));
+	return nfOps->call(NFPCI(NFPCI_VIRT_TO_BUS), device_handle, virt_cpu_address, data);
 }
 
 long bus_to_virt(unsigned long device_handle, unsigned long pci_address, void *data)
 {
-	return nfCall((NFPCI(NFPCI_BUS_TO_VIRT), device_handle, pci_address, data));
+	return nfOps->call(NFPCI(NFPCI_BUS_TO_VIRT), device_handle, pci_address, data);
 }
 
 long virt_to_phys(void *virt_cpu_address, void *data)
 {
-	return nfCall((NFPCI(NFPCI_VIRT_TO_PHYS), virt_cpu_address, data));
+	return nfOps->call(NFPCI(NFPCI_VIRT_TO_PHYS), virt_cpu_address, data);
 }
 
 long phys_to_virt(void *phys_cpu_address, void *data)
 {
-	return nfCall((NFPCI(NFPCI_PHYS_TO_VIRT), phys_cpu_address, data));
+	return nfOps->call(NFPCI(NFPCI_PHYS_TO_VIRT), phys_cpu_address, data);
 }
