d6cc0a7fe0345877468d2e0332d365abc2c7e80e
philipp <>
2006-12-31 04:55:37 +0000

    Added missing include

--- src/natfeat/nfosmesa.cpp
+++ src/natfeat/nfosmesa.cpp
@@ -18,6 +18,10 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#ifdef OS_darwin
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
 #include <SDL_loadso.h>
 #include <SDL_endian.h>
 #include <GL/osmesa.h>
