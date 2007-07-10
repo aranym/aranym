0097ace422cbaa3d17cb61d951cf8e53f29eb3ac
joy <>
2007-07-10 12:35:40 +0000

    include dyngl only if ENABLE_OPENGL is defined

--- src/host.cpp
+++ src/host.cpp
@@ -1,15 +1,33 @@
 /*
- * $Header$
+ * host.cpp
  *
- * STanda 2001
+ * Copyright (c) 2001-2007 Stanislav Opichal of ARAnyM dev team (see AUTHORS)
+ *
+ * This file is part of the ARAnyM project which builds a new and powerful
+ * TOS/FreeMiNT compatible virtual machine running on almost any hardware.
+ *
+ * ARAnyM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ARAnyM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARAnyM; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 
 #include "sysdeps.h"
 
 #include "hostscreen.h"
 #include "host.h"
-#include "dyngl.h"
+#ifdef ENABLE_OPENGL
+#  include "dyngl.h"
+#endif
 
 #define DEBUG 0
 #include "debug.h"
@@ -53,25 +71,5 @@ void Host::reset(void)
 }
 
 /*
- * $Log$
- * Revision 1.6  2006/02/03 21:21:34  pmandin
- * Created host clock class to encapsulate OS-dependent time functions
- *
- * Revision 1.5  2006/01/26 18:53:53  pmandin
- * HostAudio object now statically created
- *
- * Revision 1.4  2006/01/23 18:27:19  pmandin
- * Add reset method for host stuff
- *
- * Revision 1.3  2005/12/31 09:07:43  pmandin
- * Created a Host class, put HostAudio in it
- *
- * Revision 1.2  2001/10/25 19:56:01  standa
- * The Log and Header CVS tags in the Log removed. Was recursing.
- *
- * Revision 1.1  2001/10/08 21:46:05  standa
- * The Header and Log CVS tags added.
- *
- *
- *
- */
+vim:ts=4:sw=4:
+*/
