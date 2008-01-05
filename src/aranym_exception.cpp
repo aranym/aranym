52955f52ed93191d250c6f9676ff7cc71735abae
milan <>
2008-01-05 11:25:31 +0000

    backout previous change, not correct

--- src/aranym_exception.cpp
+++ src/aranym_exception.cpp
@@ -18,8 +18,8 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include <cstdarg>
-#include <cstdio>
+#include <stdarg.h>
+#include <stdio.h>
 
 #include "aranym_exception.h"
 
