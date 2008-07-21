432a03eeb95763f06644ba23273665c03637d136
pmandin <>
2008-07-21 16:25:47 +0000

    Fix again modulo addressing

--- src/dsp_cpu.cpp
+++ src/dsp_cpu.cpp
@@ -1249,21 +1249,20 @@ static void dsp_update_rn_bitreverse(uint32 numreg)
 
 static void dsp_update_rn_modulo(uint32 numreg, int16 modifier)
 {
-	uint16 bufsize, modulo, lobound, hibound, value;
+	uint16 bufsize, modulo, lobound, hibound, bufmask;
 	int16 r_reg;
 
 	modulo = (getDSP()->registers[DSP_REG_M0+numreg]+1) & BITMASK(16);
 	bufsize = 1;
+	bufmask = BITMASK(16);
 	while (bufsize < modulo) {
 		bufsize <<= 1;
+		bufmask <<= 1;
 	}
+	bufmask &= BITMASK(16);
 	
-	/* lobound is the highest multiple of bufsize<= Rn */
-	value = getDSP()->registers[DSP_REG_R0+numreg] & BITMASK(16);
-	value /= bufsize;
-	lobound = value*bufsize;
-
-	hibound = lobound + modulo;
+	lobound = getDSP()->registers[DSP_REG_R0+numreg] & bufmask;
+	hibound = lobound + modulo - 1;
 
 	r_reg = (int16) (getDSP()->registers[DSP_REG_R0+numreg] & BITMASK(16));
 	while (modifier>=bufsize) {
@@ -1276,9 +1275,9 @@ static void dsp_update_rn_modulo(uint32 numreg, int16 modifier)
 	}
 	r_reg += modifier;
 	if (r_reg>hibound) {
-		r_reg -= modulo+1;
+		r_reg -= modulo;
 	} else if (r_reg<lobound) {
-		r_reg += modulo+1;
+		r_reg += modulo;
 	}
 
 	getDSP()->registers[DSP_REG_R0+numreg] = ((uint32) r_reg) & BITMASK(16);
