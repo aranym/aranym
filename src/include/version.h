389641aa55377d2c8927b227263404a62be6ec76
joy <>
2008-11-11 19:49:40 +0000

    ver+

--- src/include/version.h
+++ src/include/version.h
@@ -33,8 +33,8 @@
 #ifndef VER_STATUS
 //#define VER_STATUS
 //#define VER_STATUS	"alpha"
-#define VER_STATUS	"beta"
-//#define VER_STATUS	"beta+" CVS_DATE
+//#define VER_STATUS	"beta"
+#define VER_STATUS	"beta+" CVS_DATE
 #endif
 
 #define str(x)		_stringify (x)
