1a550e73f7c995b779f75c64c2e0515d4597668a
pmandin <>
2009-03-18 20:43:32 +0000

    dsp56k: fix for update Rn with modulo

--- src/dsp_cpu.c
+++ src/dsp_cpu.c
@@ -1404,34 +1404,40 @@ static void dsp_update_rn_bitreverse(Uint32 numreg)
 static void dsp_update_rn_modulo(Uint32 numreg, Sint16 modifier)
 {
 	Uint16 bufsize, modulo, lobound, hibound, bufmask;
-	Sint16 r_reg;
+	Sint16 r_reg, orig_modifier=modifier;
 
-	modulo = (dsp_core->registers[DSP_REG_M0+numreg]+1) & BITMASK(16);
+	modulo = dsp_core->registers[DSP_REG_M0+numreg]+1;
 	bufsize = 1;
 	bufmask = BITMASK(16);
 	while (bufsize < modulo) {
 		bufsize <<= 1;
 		bufmask <<= 1;
 	}
-	bufmask &= BITMASK(16);
 	
 	lobound = dsp_core->registers[DSP_REG_R0+numreg] & bufmask;
 	hibound = lobound + modulo - 1;
 
-	r_reg = (Sint16) (dsp_core->registers[DSP_REG_R0+numreg] & BITMASK(16));
-	while (modifier>=bufsize) {
-		r_reg += bufsize;
-		modifier -= bufsize;
+	r_reg = (Sint16) dsp_core->registers[DSP_REG_R0+numreg];
+
+	if (orig_modifier>modulo) {
+		while (modifier>bufsize) {
+			r_reg += bufsize;
+			modifier -= bufsize;
+		}
+		while (modifier<-bufsize) {
+			r_reg -= bufsize;
+			modifier += bufsize;
+		}
 	}
-	while (modifier<=-bufsize) {
-		r_reg -= bufsize;
-		modifier += bufsize;
-	}
+
 	r_reg += modifier;
-	if (r_reg>hibound) {
-		r_reg -= modulo;
-	} else if (r_reg<lobound) {
-		r_reg += modulo;
+
+	if (orig_modifier!=modulo) {
+		if (r_reg>hibound) {
+			r_reg -= modulo;
+		} else if (r_reg<lobound) {
+			r_reg += modulo;
+		}	
 	}
 
 	dsp_core->registers[DSP_REG_R0+numreg] = ((Uint32) r_reg) & BITMASK(16);
