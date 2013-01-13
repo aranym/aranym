cd45208a8437cb5f0a5bb1dc90c4baa95369f9be
joy <>
2013-01-13 22:03:55 +0000

    - added an address check to NatFeat memory copy operations so guest programs should no longer be able to kill ARAnyM - disabled the direct memcpy() operations, to be 100% sure guest programs don't kill ARAnyM. Might have negative impact on NatFeats speed!

--- src/include/natfeats.h
+++ src/include/natfeats.h
@@ -1,3 +1,26 @@
+/*
+ * natfeats.h - common functions for all NatFeats
+ *
+ * Copyright (c) 2001-2013 Petr Stehlik of ARAnyM dev team (see AUTHORS)
+ *
+ * This file is part of the ARAnyM project which builds a new and powerful
+ * TOS/FreeMiNT compatible virtual machine running on almost any hardware.
+ *
+ * ARAnyM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ARAnyM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ARAnyM; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifndef _NATFEATS_H
 #define _NATFEATS_H
 
@@ -13,6 +36,9 @@ extern uint32 nf_getparameter(int);
 
 // should NatFeats work with physical (not MMU mapped) addresses
 #define NATFEAT_PHYS_ADDR	1
+
+// should NatFeats use direct memcpy() to/from guest provided pointer (dangerous, user program can kill ARAnyM)
+#define NATFEAT_LIBC_MEMCPY	0
 
 #if NATFEAT_PHYS_ADDR
 #  define ReadNFInt8	ReadAtariInt8
@@ -32,32 +58,41 @@ extern uint32 nf_getparameter(int);
 
 static inline void Atari2Host_memcpy(void *dst, memptr src, size_t n)
 {
-#if NATFEAT_PHYS_ADDR
+#if NATFEAT_LIBC_MEMCPY
+	if (! ValidAtariAddr(src, false, n))
+		BUS_ERROR(src);
+
 	memcpy(dst, Atari2HostAddr(src), n);
 #else
 	uint8 *dest = (uint8 *)dst;
 	while ( n-- )
-		*dest++ = (char)ReadInt8( (uint32)src++ );
+		*dest++ = (char)ReadNFInt8( (uint32)src++ );
 #endif
 }
 
 static inline void Host2Atari_memcpy(memptr dest, const void *src, size_t n)
 {
-#if NATFEAT_PHYS_ADDR
+#if NATFEAT_LIBC_MEMCPY
+	if (! ValidAtariAddr(dest, true, n))
+		BUS_ERROR(dest);
+
 	memcpy(Atari2HostAddr(dest), src, n);
 #else
 	uint8 *source = (uint8 *)src;
 	while ( n-- )
-		WriteInt8( dest++, *source++ );
+		WriteNFInt8( dest++, *source++ );
 #endif
 }
 
-static inline void Atari2HostSafeStrncpy( char *dest, memptr source, size_t count )
+static inline void Atari2HostSafeStrncpy(char *dest, memptr source, size_t count)
 {
-#if NATFEAT_PHYS_ADDR
+#if NATFEAT_LIBC_MEMCPY
+	if (! ValidAtariAddr(source, false, count))
+		BUS_ERROR(source);
+
 	safe_strncpy(dest, (const char*)Atari2HostAddr(source), count);
 #else
-	while ( count > 1 && (*dest = (char)ReadInt8( source++ )) != 0 ) {
+	while ( count > 1 && (*dest = (char)ReadNFInt8( source++ )) != 0 ) {
 		count--;
 		dest++;
 	}
@@ -66,17 +101,20 @@ static inline void Atari2HostSafeStrncpy( char *dest, memptr source, size_t coun
 #endif
 }
 
-static inline void Host2AtariSafeStrncpy( memptr dest, const char *source, size_t count )
+static inline void Host2AtariSafeStrncpy(memptr dest, const char *source, size_t count)
 {
-#if NATFEAT_PHYS_ADDR
+#if NATFEAT_LIBC_MEMCPY
+	if (! ValidAtariAddr(dest, true, count))
+		BUS_ERROR(dest);
+
 	safe_strncpy((char *)Atari2HostAddr(dest), source, count);
 #else
 	while ( count > 1 && *source ) {
-		WriteInt8( dest++, (uint8)*source++ );
+		WriteNFInt8( dest++, (uint8)*source++ );
 		count--;
 	}
 	if (count > 0)
-		WriteInt8( dest, 0 );
+		WriteNFInt8( dest, 0 );
 #endif
 }
 #endif /* _NATFEATS_H */
