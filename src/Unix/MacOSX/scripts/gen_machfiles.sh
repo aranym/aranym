b326f01501e4ad7232a12d4b66333d7578747dca
Jens Heitmann <jens.heitmann@stc-de.com>
2014-05-13 21:23:20 +0200

    gen_machfiles.sh prepared for XCode5 / Mavericks; Fixed typo

--- src/Unix/MacOSX/scripts/gen_machfiles.sh
+++ src/Unix/MacOSX/scripts/gen_machfiles.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 ########################## GENERATE MACH FILES FROM SYSTEM DEFS ##########################
+export >/Users/jens/Desktop/export.txt
 
 if [[ $ARCHS =~ (^| )x86_64($| ) && ! -f $BUILD_DIR/mach_excServer.c ]]; then
 	pushd "$BUILD_DIR"
-	mig -arch x86_64 -v /usr/include/mach/mach_exc.defs
+
+	DEFS_FILE=/usr/include/mach/mach_exc.defs
+	if [[! -f $DEFS_FILE]]; then
+		# In XCode 5 SDK is located inside the XCode Application package
+		DEFS_FILE=$SDK_DIR/usr/include/mach/mach_exc.defs
+	fi
+
+	mig -arch x86_64 -v $DEFS_FILE
 	popd
 fi
