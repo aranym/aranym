1d0712f33298b39d045c2745fc5e0947cec78f69
Jens Heitmann <jens.heitmann@stc-de.com>
2014-05-14 11:37:56 +0200

    Fixed debug code

--- src/Unix/MacOSX/scripts/gen_machfiles.sh
+++ src/Unix/MacOSX/scripts/gen_machfiles.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 ########################## GENERATE MACH FILES FROM SYSTEM DEFS ##########################
-export >/Users/jens/Desktop/export.txt
 
 if [[ $ARCHS =~ (^| )x86_64($| ) && ! -f $BUILD_DIR/mach_excServer.c ]]; then
 	pushd "$BUILD_DIR"
 
 	DEFS_FILE=/usr/include/mach/mach_exc.defs
 	if [[! -f $DEFS_FILE]]; then
-		# In XCode 5 SDK is located inside the XCode Application package
+		#
+		# In XCode 5 SDK is located inside the XCode Application package, and if
+		# the command line tools are not installed. To install command line tools
+		# execute xcode-select --install
+		#
 		DEFS_FILE=$SDK_DIR/usr/include/mach/mach_exc.defs
 	fi
 
