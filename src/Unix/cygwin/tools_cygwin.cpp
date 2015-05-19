2835742cc604702aaa7565933af50c32459f9e59
Thorsten Otto <thotto@users.sourceforge.net>
2015-05-19 17:49:13 +0200

    get_home_dir: Remove unused function

--- src/Unix/cygwin/tools_cygwin.cpp
+++ src/Unix/cygwin/tools_cygwin.cpp
@@ -1,11 +1,10 @@
+#include "sysdeps.h"
+
 #ifdef OS_cygwin
-#ifndef _CYGWIN_TOOLS_H
-#define _CYGWIN_TOOLS_H
 
 #include "tools.h"
 #include <sys/cygwin.h>
 #include <cygwin/version.h>
-#include <windows.h>
 
 char *cygwin_path_to_win32(char *path, size_t size)
 {
@@ -20,39 +19,4 @@ char *cygwin_path_to_win32(char *path, size_t size)
 	return path;
 }
 
-int get_home_dir(char *path, int len)
-{
-	LONG r;
-	HKEY hkResult;
-	DWORD cbData;
-
-	r = RegOpenKeyEx
-		(
-			HKEY_CURRENT_USER,
-			"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders",
-			(DWORD)0,
-			KEY_READ,
-			&hkResult
-		);
-	if (r != ERROR_SUCCESS)
-		return 1;
-
-	cbData = (DWORD)(len/(sizeof(BYTE)));
-
-	r = RegQueryValueEx
-		(
-			hkResult,
-			"Personal", 
-			NULL,
-			NULL,
-			(LPBYTE)path,
-			&cbData
-		);
-	if (r != ERROR_SUCCESS)
-		return 1;
-
-	return 0;
-}
-
-#endif /* _CYGWIN_TOOLS_H */
 #endif /* OS_cygwin */
