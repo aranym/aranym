fa9a12fde7e91a61241c4f923a0f7d2cd8215fa3
Thorsten Otto <admin@tho-otto.de>
2017-08-15 20:06:47 +0200

    Move the commands that fix the runtime library path from the xcode project file into a separate script

---
+++ src/Unix/MacOSX/scripts/set_rpath.sh
@@ -0,0 +1,26 @@
+# Fix up dynamic libraries
+
+cd "$TARGET_BUILD_DIR/$EXECUTABLE_FOLDER_PATH"
+ls -al
+test -x "$EXECUTABLE_NAME" || exit 1
+
+# Fixup libmpfr
+if test -f libmpfr.dylib -a -f libgmp.dylib; then
+	for i in "" .10; do
+		install_name_tool -change /opt/local/lib/libgmp${i}.dylib @executable_path/libgmp.dylib libmpfr.dylib || exit 1
+	done
+fi
+
+# Fixup executable
+if test -f libgmp.dylib; then
+	for i in "" .10; do
+		install_name_tool -change /opt/local/lib/libgmp${i}.dylib @executable_path/libgmp.dylib "$EXECUTABLE_NAME" || exit 1
+	done
+fi
+if test -f libmpfr.dylib; then
+	for i in "" .4; do
+		install_name_tool -change /opt/local/lib/libmpfr${i}.dylib @executable_path/libmpfr.dylib "$EXECUTABLE_NAME" || exit 1
+	done
+fi
+
+exit 0
