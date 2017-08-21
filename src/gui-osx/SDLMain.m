c7d5f6d0075c3204ebe68dc0922d1f59cd47f913
Thorsten Otto <admin@tho-otto.de>
2017-08-21 03:15:39 +0200

    Mac OS X 10.9 no longer uses the -psn_XXX command line arg for Finder launches.

--- src/gui-osx/SDLMain.m
+++ src/gui-osx/SDLMain.m
@@ -228,12 +228,35 @@ void SDL_GL_SetCurrentContext(SDL_GLContext ctx)
 #endif
 
 
+static int IsRootCwd(void)
+{
+    char buf[MAXPATHLEN];
+    char *cwd = getcwd(buf, sizeof (buf));
+    return cwd != NULL && strcmp(cwd, "/") == 0;
+}
+
+static int IsFinderLaunch(int argc, char **argv)
+{
+    /* -psn_XXX is passed if we are launched from Finder, SOMETIMES */
+    if ( (argc >= 2) && (strncmp(argv[1], "-psn", 4) == 0) ) {
+        return 1;
+    } else if ((argc == 1) && IsRootCwd()) {
+        /* we might still be launched from the Finder; on 10.9+, you might not
+        get the -psn command line anymore. If there's no
+        command line, and if our current working directory is "/", it
+        might as well be a Finder launch. */
+        return 1;
+    }
+    return 0;  /* not a Finder launch. */
+}
+
+
 /* Main entry point to executable - should *not* be SDL_main! */
 int main (int argc, char **argv)
 {
 	/* Copy the arguments into a global variable */
 	/* This is passed if we are launched by double-clicking */
-	if ( argc >= 2 && strncmp (argv[1], "-psn", 4) == 0 ) 
+	if (IsFinderLaunch(argc, argv))
 	{
 		gArgv = (char **) SDL_malloc(sizeof (char *) * 2);
 		gArgv[0] = argv[0];
