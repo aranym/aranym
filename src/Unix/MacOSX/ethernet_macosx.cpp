aranym/aranym

commit b839b37fb55dfa9f4c8323cadfe3990bf3997891

Don't rely on current directory being writable when creating temporary config file

src/Unix/MacOSX/ethernet_macosx.cpp
@@ -226,9 +226,11 @@ bool BPFEthernetHandler::open()
 	 ******************************************************/
 	char exe_path[2048];
 	CFURLRef url = CFBundleCopyBundleURL(CFBundleGetMainBundle());
-	CFURLGetFileSystemRepresentation(url, true, (UInt8 *)exe_path, sizeof(exe_path));
+	CFURLRef url2 = CFURLCreateCopyDeletingLastPathComponent(0, url);
+	CFURLGetFileSystemRepresentation(url2, true, (UInt8 *)exe_path, sizeof(exe_path));
+	CFRelease(url2);
 	CFRelease(url);
-	strcat(exe_path, "/Contents/MacOS/"ETH_HELPER);
+	addFilename(exe_path, ETH_HELPER, sizeof(exe_path));
 	if (debug)
 	{
 		D(bug("BPF(%d): starting helper from <%s>", ethX, exe_path));

src/cfgopts.cpp
@@ -710,28 +710,37 @@ bool ConfigOptions::write_token(FILE *outfile, struct Config_Tag *ptr)
 /-------------------------------------------------------------------<<*/
 int	ConfigOptions::update_config(struct Config_Tag configs[], const char *header)
 {
-#ifdef OS_darwin
-	static const char *tempfilename = "/tmp/aratemp.$$$";
-#else
-	static const char *tempfilename = "temp.$$$";
-#endif
+	static const char *const tempfilenames[] = {
+		"/tmp/aratemp.$$$",
+		"aratemp.$$$",
+		NULL
+	};
+	const char *tempfilename;
 	struct Config_Tag *ptr;
 	int	count = 0, lineno = 0;
 	FILE * infile, *outfile;
 	char	*fptr, *tok;
 	int result = 0;
-
+	int i;
+	
 	for ( ptr = configs; ptr->buf; ++ptr )
 		ptr->stat = 0;	/* jeste neulozeno do souboru */
 
-	infile = fopen(config_file, "rt");
+	outfile = NULL;
+	i = 0;
+	while ((tempfilename = tempfilenames[i++]) != NULL)
+	{
+		outfile = fopen(tempfilename, "w");
+		if (outfile != NULL)
+			break;
+	}
+	if ( outfile == NULL ) {
+		panicbug("Error: unable to open %s file.", tempfilenames[0]);
+		return ERROR;		/* return error designation. */
+	}
+	infile = fopen(config_file, "r");
 	if ( infile == NULL ) {
 /* konfiguracni soubor jeste vubec neexistuje */
-		outfile = fopen(tempfilename, "wt");
-		if ( outfile == NULL ) {
-			panicbug("Error: unable to open %s file.", tempfilename);
-			return ERROR;		/* return error designation. */
-		}
 		if ( header != NULL ) {
 			fprintf(outfile, "%s\n", header);
 		}
@@ -749,12 +758,6 @@ int	ConfigOptions::update_config(struct Config_Tag configs[], const char *header
 			return result;
 		}
 		return count;
-	}
-	outfile = fopen(tempfilename, "wt");
-	if ( outfile == NULL ) {
-		panicbug("Error: unable to open %s file.", tempfilename);
-		fclose(infile);
-		return ERROR;		   /* return error designation. */
 	}
 	if ( header != NULL ) {
 /* konfiguracni soubor existuje a je otevren - hledame nasi sekci */

src/hostscreen.cpp
@@ -24,6 +24,7 @@
 #endif
 
 #include "newcpu.h"
+#include <errno.h>
 
 #include "SDL_compat.h"
 #include <vector>

commit 0ffd7e6187fbb06957c9851cd6090a7725d0c6aa

Fix bug introduced in commit b839b37f causing the bpf_helper executable not to be found

src/Unix/MacOSX/ethernet_macosx.cpp
@@ -225,7 +225,7 @@ bool BPFEthernetHandler::open()
 	 Fork child process to get an open BPF file descriptor
 	 ******************************************************/
 	char exe_path[2048];
-	CFURLRef url = CFBundleCopyBundleURL(CFBundleGetMainBundle());
+	CFURLRef url = CFBundleCopyExecutableURL(CFBundleGetMainBundle());
 	CFURLRef url2 = CFURLCreateCopyDeletingLastPathComponent(0, url);
 	CFURLGetFileSystemRepresentation(url2, true, (UInt8 *)exe_path, sizeof(exe_path));
 	CFRelease(url2);