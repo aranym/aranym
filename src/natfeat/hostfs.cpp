1cdd45d4c77f23ccfc8e81d7dd95d674bfdd6c54
Thorsten Otto <admin@tho-otto.de>
2021-03-04 08:42:36 +0100

    Workaround for FreeBSD, which is lacking the global timezone variable

--- src/natfeat/hostfs.cpp
+++ src/natfeat/hostfs.cpp
@@ -2894,7 +2894,22 @@ int32 HostFs::xfs_native_init( int16 devnum, memptr mountpoint, memptr hostroot,
 	 * for drivers that are not running under mint,
 	 * report our current timezone (in the filesys.res1 field)
 	 */
+#if defined(__FreeBSD__) || 1
+	/*
+	 * FreeBSD does not have that global variable.
+	 * Maybe others, too.
+	 */
+	{
+		time_t t;
+		int32_t offset;
+		
+		t = time(0);
+		offset = -gmtoff(t);
+		WriteInt32(filesys + 136, offset);
+	}
+#else
 	WriteInt32(filesys + 136, timezone);
+#endif
 
 	int16 dnum = -1;
 	size_t len = strlen( fmountPoint );
