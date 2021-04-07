e577eb5c425cfad70884a1580f9d5b78bde7b24b
Philipp <pd95@users.noreply.github.com>
2021-04-07 23:38:24 +0200

    Improve fixup script - use `otool` to find platform specific path which needs fixup

--- src/Unix/MacOSX/scripts/set_rpath.sh
+++ src/Unix/MacOSX/scripts/set_rpath.sh
@@ -6,25 +6,37 @@ test -x "$EXECUTABLE_NAME" || exit 1
 
 # Fixup libmpfr
 if test -f libmpfr.dylib -a -f libgmp.dylib; then
-	for i in "" .10; do
-		install_name_tool -change /opt/local/lib/libgmp${i}.dylib @executable_path/libgmp.dylib libmpfr.dylib || exit 1
+	echo "Fixup libmpfr: libgmp"
+	for ARCH in $ARCHS ; do
+		ORIG_PATH=`otool -arch $ARCH -l "libmpfr.dylib" | grep libgmp | awk '{print $2}'`
+		echo "  $ARCH: $ORIG_PATH"
+		install_name_tool -change "$ORIG_PATH" @executable_path/libgmp.dylib libmpfr.dylib || exit 1
 	done
 fi
 
 # Fixup executable
 if test -f libgmp.dylib; then
-	for i in "" .10; do
-		install_name_tool -change /opt/local/lib/libgmp${i}.dylib @executable_path/libgmp.dylib "$EXECUTABLE_NAME" || exit 1
+	echo "Fixup $EXECUTABLE_NAME: libgmp"
+	for ARCH in $ARCHS ; do
+		ORIG_PATH=`otool -arch $ARCH -l "$EXECUTABLE_NAME" | grep libgmp | awk '{print $2}'`
+		echo "  $ARCH: $ORIG_PATH"
+		install_name_tool -change "$ORIG_PATH" @executable_path/libgmp.dylib "$EXECUTABLE_NAME" || exit 1
 	done
 fi
 if test -f libmpfr.dylib; then
-	for i in "" .4; do
-		install_name_tool -change /opt/local/lib/libmpfr${i}.dylib @executable_path/libmpfr.dylib "$EXECUTABLE_NAME" || exit 1
+	echo "Fixup $EXECUTABLE_NAME: libmpfr"
+	for ARCH in $ARCHS ; do
+		ORIG_PATH=`otool -arch $ARCH -l "$EXECUTABLE_NAME" | grep libmpfr | awk '{print $2}'`
+		echo "  $ARCH: $ORIG_PATH"
+		install_name_tool -change "$ORIG_PATH" @executable_path/libmpfr.dylib "$EXECUTABLE_NAME" || exit 1
 	done
 fi
 if test -f libjpeg.dylib; then
-	for i in "" .7 .8 .9; do
-		install_name_tool -change /opt/local/lib/libjpeg${i}.dylib @executable_path/libjpeg.dylib "$EXECUTABLE_NAME" || exit 1
+	echo "Fixup $EXECUTABLE_NAME: libjpeg"
+	for ARCH in $ARCHS ; do
+		ORIG_PATH=`otool -arch $ARCH -l "$EXECUTABLE_NAME" | grep libjpeg | awk '{print $2}'`
+		echo "  $ARCH: $ORIG_PATH"
+		install_name_tool -change "$ORIG_PATH" @executable_path/libjpeg.dylib "$EXECUTABLE_NAME" || exit 1
 	done
 fi
 
